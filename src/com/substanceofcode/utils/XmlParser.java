9f9c939f53a675122c4c53e522a453d873f217c4
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2007-08-14 19:34:01 +0000

    Support UTF-16, CP1252, ISO8859-1 and variants.

--- src/com/substanceofcode/utils/XmlParser.java
+++ src/com/substanceofcode/utils/XmlParser.java
@@ -20,12 +20,19 @@
  *
  */
 
+// Expand to define logging define
+//#define DNOLOGGING
 package com.substanceofcode.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
+//#ifdef DLOGGING
+//@import net.sf.jlogmicro.util.logging.Logger;
+//@import net.sf.jlogmicro.util.logging.LogManager;
+//@import net.sf.jlogmicro.util.logging.Level;
+//#endif
 /**
  * Simple and lightweight XML parser without complete error handling.
  *
@@ -33,96 +40,141 @@ import java.io.UnsupportedEncodingException;
  */
 public class XmlParser {
     
-    private InputStream m_inputStream = null;
-    
     /** Current XML element name (eg. <title> = title) */
     private String m_currentElementName = "";
     private String m_currentElementData = "";
     private boolean m_currentElementContainsText = false;
-    private static String encoding = "UTF-8";
+    private String fileEncoding = "ISO8859_1";  // See EncodingUtil
+    private String docEncoding = "";  // See EncodingUtil
+    private EncodingUtil encodingUtil = null;
     private String namespace = null;
+    private boolean getPrologue = true;
+    private int nextChar;
+	//#ifdef DLOGGING
+//@    private Logger logger = Logger.getLogger("XmlParser");
+//@    private boolean fineLoggable = logger.isLoggable(Level.FINE);
+//@    private boolean finerLoggable = logger.isLoggable(Level.FINER);
+//@    private boolean finestLoggable = logger.isLoggable(Level.FINEST);
+	//#endif
     
     /** Enumerations for parse function */
     public static final int END_DOCUMENT = 0;
     public static final int ELEMENT = 1;
+    public static final int PROLOGUE = 2;
     
     /** Creates a new instance of XmlParser */
     public XmlParser(InputStream inputStream) {
-        m_inputStream = inputStream;
+		encodingUtil = new EncodingUtil(inputStream);
+		fileEncoding = encodingUtil.getFileEncoding();
     }
-    
+
     /** Parse next element */
     public int parse() throws IOException {
-        StringBuffer inputBuffer = new StringBuffer();
-        
-        boolean parsingElementName = false;
-        boolean elementFound = false;
-        boolean elementStart = false;
-        boolean parsingElementData = false;
-                
-        int inputCharacter;
+		StringBuffer inputBuffer = new StringBuffer();
+		
+		boolean parsingElementName = false;
+		boolean elementFound = false;
+		boolean elementStart = false;
+		boolean parsingElementData = false;
+		boolean prologueFound = false;
+				
         char c;
-        inputCharacter = m_inputStream.read();
-        while (inputCharacter != -1 && elementFound==false) {
-            c = (char)inputCharacter;
-            
-            if(c=='/' && elementStart) {
-                parsingElementName = false;
-            }
-            else if(elementStart && (c=='?' || c=='!')) {
-                if(m_currentElementData.charAt(m_currentElementData.length()-1)=='<') {
-                    parsingElementName = false;
-                }
-            }
-            if(parsingElementName) {
-                if(c==' ' || c=='/' || c==':') {
-					// For specified namespace, put it into element name
-					if ((c==':') && (namespace != null) &&
-						namespace.equals(m_currentElementName)) {
-						m_currentElementName += c;
-					} else {
+        int inputCharacter = encodingUtil.read();
+		try {
+			while ((inputCharacter != -1) && !elementFound) {
+                c = (char)inputCharacter;
+				
+				if(c=='/' && elementStart) {
+					parsingElementName = false;
+				}
+				else if(elementStart && (c=='?' || c=='!')) {
+					if(m_currentElementData.charAt(m_currentElementData.length()-1)=='<') {
 						parsingElementName = false;
-						parsingElementData = true;
+						// If we find <? and we're looking for the prologue,
+						// set flag.
+						if (getPrologue) {
+							prologueFound = true;
+						}
 					}
-                }
-                else if(c!='>') {
-                    m_currentElementName += c;
-                }
-            }              
-            if(c=='<') {
-                elementStart = true;
-                parsingElementName = true;
-                parsingElementData = true;
-                m_currentElementName = "";
-                m_currentElementData = "";
-            }            
-            if(parsingElementData) {
-                m_currentElementData += c;
-            }
-            if(c=='>') {
-                if(m_currentElementName.length()>0) {
-                    elementFound = true;
-                    parsingElementName = false;
-                }
-            }    
+				}
+				if(parsingElementName) {
+					if(c==' ' || c=='/' || c==':') {
+						// For specified namespace, put it into element name
+						if ((c==':') && (namespace != null) &&
+							namespace.equals(m_currentElementName)) {
+							m_currentElementName += c;
+						} else {
+							parsingElementName = false;
+							parsingElementData = true;
+						}
+					}
+					else if(c!='>') {
+						m_currentElementName += c;
+					}
+				}              
+				if(c=='<') {
+					elementStart = true;
+					parsingElementName = true;
+					parsingElementData = true;
+					m_currentElementName = "";
+					m_currentElementData = "";
+				}            
+				if(parsingElementData) {
+					m_currentElementData += c;
+				}
+				if(c=='>') {
+					if(m_currentElementName.length()>0) {
+						elementFound = true;
+						parsingElementName = false;
+						// If we find XML without a prologue, need
+						// to treat as default UTF-8 encoding for XML.
+						if (getPrologue) {
+							getPrologue = false;
+							encodingUtil.getEncoding(null);
+							docEncoding = encodingUtil.getDocEncoding();
+						}
+					} else if (getPrologue && prologueFound) {
+						// If we are looking for the prolog, now
+						// we have read the end of it, so we can
+						// get the encoding specified (or null which
+						// defaults to UTF-8).
+						getPrologue = false;
+						String cencoding = getAttributeValue("encoding");
+						encodingUtil.getEncoding(cencoding);
+						// Get doc encoding.  The encoding to translate
+						// the bytes into.
+						docEncoding = encodingUtil.getDocEncoding();
+						return PROLOGUE;
+					}
+				}    
 
-            if(!elementFound){
-                inputCharacter = m_inputStream.read();
-            }
-        }
-        
-        if( m_currentElementData.charAt( m_currentElementData.length()-2 )=='/' &&
-            m_currentElementData.charAt( m_currentElementData.length()-1 )=='>' ) {
-            m_currentElementContainsText = false;
-        } else {
-            m_currentElementContainsText = true;
-        }
-        
-        if( inputCharacter==-1 ) {
-            return END_DOCUMENT;
-        } else {
-            return ELEMENT;
-        }
+				if(!elementFound){
+                    inputCharacter = encodingUtil.read();
+				}
+			}
+			
+			//#ifdef DLOGGING
+//@			if (finerLoggable) {logger.finer("m_currentElementData=" + m_currentElementData);}
+			//#endif
+			if( m_currentElementData.charAt( m_currentElementData.length()-2 )=='/' &&
+				m_currentElementData.charAt( m_currentElementData.length()-1 )=='>' ) {
+				m_currentElementContainsText = false;
+			} else {
+				m_currentElementContainsText = true;
+			}
+			
+		} catch (IOException e) {
+//#ifdef DLOGGING
+//@			logger.severe("parse read error ");
+//#endif
+			System.out.println("parse read error " + e + " " + e.getMessage());
+			throw e;
+		}
+		if( inputCharacter == -1 ) {
+			return END_DOCUMENT;
+		} else {
+			return ELEMENT;
+		}
     }
     
     /** Get element name */
@@ -133,103 +185,136 @@ public class XmlParser {
     /** Get element text including inner xml */
     public String getText() throws IOException {
         
-        if(m_currentElementContainsText==false) {
-            return "";
-        }
-        boolean endParsing = false;
-        
-        String endElementName = "";
-        String text;
-        StringBuffer textBuffer = new StringBuffer();
-        int inputCharacter;
-        char c;
-        char lastChars[] = new char[3];
-        lastChars[0] = ' ';
-        lastChars[1] = ' ';
-        lastChars[2] = ' ';
-        
-        char elementNameChars[] = new char[3];
-        elementNameChars[0] = m_currentElementName.charAt( m_currentElementName.length()-3 );
-        elementNameChars[1] = m_currentElementName.charAt( m_currentElementName.length()-2 );
-        elementNameChars[2] = m_currentElementName.charAt( m_currentElementName.length()-1 );
-        while ((inputCharacter = m_inputStream.read()) != -1 && endParsing==false) {
-            c = (char)inputCharacter;
-            lastChars[0] = lastChars[1];
-            lastChars[1] = lastChars[2];
-            lastChars[2] = c;
-            //System.out.print(c);
+		if(!m_currentElementContainsText) {
+			return "";
+		}
+		boolean endParsing = false;
+		
+		String endElementName = "";
+		String text = null;
+		try {
+			StringBuffer textBuffer = new StringBuffer();
+			int inputCharacter;
+			char c;
+			char lastChars[] = new char[3];
+			lastChars[0] = ' ';
+			lastChars[1] = ' ';
+			lastChars[2] = ' ';
+			
+			char elementNameChars[] = new char[3];
+			elementNameChars[0] = m_currentElementName.charAt( m_currentElementName.length()-3 );
+			elementNameChars[1] = m_currentElementName.charAt( m_currentElementName.length()-2 );
+			elementNameChars[2] = m_currentElementName.charAt( m_currentElementName.length()-1 );
+			while (((inputCharacter = encodingUtil.read()) != -1) &&
+					!endParsing) {
+				c = (char)inputCharacter;
+				lastChars[0] = lastChars[1];
+				lastChars[1] = lastChars[2];
+				lastChars[2] = c;
+				//System.out.print(c);
 
-            textBuffer.append(c);
-            if( lastChars[0] == elementNameChars[0] &&
-                lastChars[1] == elementNameChars[1] &&
-                lastChars[2] == elementNameChars[2]) {
-                if( textBuffer.toString().endsWith("</" + m_currentElementName)) {
-                    endParsing = true;
-                }
-            }
-        }
-		if (encoding.equals("")) {
-			text = textBuffer.toString();
-		} else {
-			try {
-				text = new String(textBuffer.toString().getBytes(), encoding);
-			} catch (UnsupportedEncodingException e) {
-				try {
-					text = new String(textBuffer.toString().getBytes(), "UTF8");
-					encoding = "UTF8";
-				} catch (UnsupportedEncodingException e2) {
-					text = textBuffer.toString();
-					encoding = "";
+				textBuffer.append(c);
+				if( lastChars[0] == elementNameChars[0] &&
+					lastChars[1] == elementNameChars[1] &&
+					lastChars[2] == elementNameChars[2]) {
+					if( textBuffer.toString().endsWith("</" + m_currentElementName)) {
+						endParsing = true;
+					}
 				}
 			}
+
+			if (docEncoding.equals("")) {
+				text = textBuffer.toString();
+			} else {
+				// We read the bytes in as ISO8859_1, so we must get them
+				// out as that and then encode as they should be.
+				if (fileEncoding.equals("")) {
+					text = new String(textBuffer.toString().getBytes(),
+									  docEncoding);
+				} else {
+					text = new String(textBuffer.toString().getBytes(
+								fileEncoding), docEncoding);
+				}
+			}
+			text = StringUtil.replace(text, "</" + m_currentElementName, "");
+			
+			/** Handle some entities and encoded characters */
+			text = StringUtil.replace(text, "<![CDATA[", "");
+			text = StringUtil.replace(text, "]]>", "");
+			text = replaceAlphaEntities(text);
+			// No need to convert from UTF-8 to Unicode using replace
+			// umlauts now because it is done with new String...,encoding.
+
+			// Replace numeric entities including &#8217;, &#8216;
+			// &#8220;, and &#8221;
+			text = encodingUtil.replaceNumEntity(text);
+
+			// Replace special chars like left quote, etc.
+			text = encodingUtil.replaceSpChars(text);
+			
+		} catch (Throwable t) {
+//#ifdef DLOGGING
+//@			logger.severe("getText Could not read a char run time.", t);
+//#endif
+			System.out.println("getText Could not read a char run time." + t +
+					           " " + t.getMessage());
 		}
-        text = StringUtil.replace(text, "</" + m_currentElementName, "");
-        
-        /** Handle some entities and encoded characters */
-        text = StringUtil.replace(text, "<![CDATA[", "");
-        text = StringUtil.replace(text, "]]>", "");
-        text = StringUtil.replace(text, "&lt;", "<");
-        text = StringUtil.replace(text, "&gt;", ">");
-        text = StringUtil.replace(text, "&nbsp;", " ");
-        text = StringUtil.replace(text, "&amp;", "&");
-        text = StringUtil.replace(text, "&apos;", "'");
-        text = StringUtil.replace(text, "&auml;", "ä");
-        text = StringUtil.replace(text, "&ouml;", "ö");
-        text = StringUtil.replace(text, "Ã¤", "ä");
-        text = StringUtil.replace(text, "Ã¶", "ö");
-        text = StringUtil.replace(text, "&#8217;", "'");
-        text = StringUtil.replace(text, "&#8216;", "'");
-        text = StringUtil.replace(text, "&#8220;", "\"");
-        text = StringUtil.replace(text, "&#8221;", "\"");
-        text = StringUtil.replace(text, "â‚¬", "€");
-        
-        return text;
+		//#ifdef DLOGGING
+//@		if (finerLoggable) {logger.finer("text=" + text);}
+		//#endif
+		return text;
     }
+
+	/**
+	  Replace alphabetic entities.
+	  */
+	public static String replaceAlphaEntities(String text) {
+		text = StringUtil.replace(text, "&lt;", "<");
+		text = StringUtil.replace(text, "&gt;", ">");
+		text = StringUtil.replace(text, "&nbsp;", " ");
+		text = StringUtil.replace(text, "&amp;", "&");
+		text = StringUtil.replace(text, "&apos;", "'");
+		text = StringUtil.replace(text, "&quot;", "\"");
+		// Avoid problems with different editors by using
+		// static variables.
+		text = StringUtil.replace(text, "&auml;", EncodingUtil.A_UMLAUTE);
+		text = StringUtil.replace(text, "&ouml;", EncodingUtil.O_UMLAUTE);
+		return text;
+	}
 
     /** 
      * Get attribute value from current element 
      */
     public String getAttributeValue(String attributeName) {
         
-        /** Check whatever the element contains given attribute */
-        int attributeStartIndex = m_currentElementData.indexOf(attributeName);
-        if( attributeStartIndex<0 ) {
-            return null;
-        }
-        
-        /** Calculate actual value start index */
-        int valueStartIndex = attributeStartIndex + attributeName.length() + 2;
-        
-        /** Check the attribute value end index */
-        int valueEndIndex = m_currentElementData.indexOf("\"", valueStartIndex);
-        if( valueEndIndex<0 ) {
-            return null;
-        }
-        
-        /** Parse value */
-        String value = m_currentElementData.substring(valueStartIndex, valueEndIndex);
-                
-        return value;
+		try {
+			/** Check whatever the element contains given attribute */
+			int attributeStartIndex = m_currentElementData.indexOf(attributeName);
+			if( attributeStartIndex<0 ) {
+				return null;
+			}
+			
+			/** Calculate actual value start index */
+			int valueStartIndex = attributeStartIndex + attributeName.length() + 2;
+			
+			/** Check the attribute value end index */
+			int valueEndIndex = m_currentElementData.indexOf("\"", valueStartIndex);
+			if( valueEndIndex<0 ) {
+				return null;
+			}
+			
+			/** Parse value */
+			String value = m_currentElementData.substring(valueStartIndex, valueEndIndex);
+					
+			return value;
+		} catch (Throwable t) {
+//#ifdef DLOGGING
+//@			logger.severe("getAttributeValue error.", t);
+//#endif
+			System.out.println("getAttributeValue error." + t + " " +
+					           t.getMessage());
+			return null;
+		}
     }
     
     public void setNamespace(String namespace) {
