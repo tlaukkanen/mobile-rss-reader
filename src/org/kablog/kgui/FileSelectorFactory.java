f1bde64d512b99cbcc181f33f1cf6ec379db72e4
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2007-10-02 10:23:25 +0000

    Allow finding files in memory for RSS and OPML.

---
+++ src/org/kablog/kgui/FileSelectorFactory.java
@@ -0,0 +1,16 @@
+/* From kablog-j2me. */
+package org.kablog.kgui;
+
+public class FileSelectorFactory {
+
+	//#ifdef JSR75
+//@	public static KFileSelector getInstance() {
+//@		
+//@		KFileSelector newInst = null;
+//@		
+//@		newInst = new KFileSelectorImpl();
+//@		
+//@		return newInst;
+//@	}
+	//#endif
+}
