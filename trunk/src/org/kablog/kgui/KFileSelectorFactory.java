973220c93ecc2ab2ce72f69c3e6b0b725ad886bb
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2007-10-02 10:23:25 +0000

    Allow finding files in memory for RSS and OPML.

---
+++ trunk/src/org/kablog/kgui/KFileSelectorFactory.java
@@ -0,0 +1,66 @@
+/* From kablog-j2me. */
+
+// Expand to define DJSR75 define
+//#define DNOJSR75
+// Expand to define logging define
+//#define DNOLOGGING
+
+package org.kablog.kgui;
+
+import javax.microedition.midlet.MIDlet;
+
+//#ifdef DLOGGING
+//@import net.sf.jlogmicro.util.logging.Logger;
+//@import net.sf.jlogmicro.util.logging.LogManager;
+//@import net.sf.jlogmicro.util.logging.Level;
+//#endif
+
+public class KFileSelectorFactory {
+	//#ifdef DLOGGING
+//@    private Logger logger = Logger.getLogger("KFileSelectorImpl");
+//@    private boolean fineLoggable = logger.isLoggable(Level.FINE);
+//@    private boolean finerLoggable = logger.isLoggable(Level.FINER);
+//@    private boolean finestLoggable = logger.isLoggable(Level.FINEST);
+	//#endif
+
+	//#ifdef DJSR75
+//@	public static KFileSelector getInstance(MIDlet midlet, String title,
+//@											String defaultDir, String iconDir) {
+//@		try {
+//@			KFileSelector newInst = new KFileSelectorImpl();
+//@			((KFileSelectorImpl)newInst).init(midlet, title, defaultDir,
+//@					iconDir);
+//@			return (newInst);
+//@		} catch (Throwable t) {
+			//#ifdef DLOGGING
+//@			Logger logger = Logger.getLogger("KFileSelectorImpl");
+//@			logger.severe("KFileSelectorFactory getInstance ", t);
+			//#endif
+//@			/** Error while executing constructor */
+//@			System.out.println("KFileSelectorFactory getInstance " + t.getMessage());
+//@			t.printStackTrace();
+//@			return null;
+//@		}
+//@
+//@	}
+//@
+//@	public static KFileSelector getInstance() {
+//@		
+//@		try {
+//@			KFileSelector newInst = new KFileSelectorImpl();
+//@			((KFileSelectorImpl)newInst).init();
+//@			return (newInst);
+//@		} catch (Throwable t) {
+			//#ifdef DLOGGING
+//@			Logger logger = Logger.getLogger("KFileSelectorImpl");
+//@			logger.severe("KFileSelectorFactory getInstance ", t);
+			//#endif
+//@			/** Error while executing constructor */
+//@			System.out.println("KFileSelectorFactory getInstance " + t.getMessage());
+//@			t.printStackTrace();
+//@			return null;
+//@		}
+//@
+//@	}
+	//#endif
+}
