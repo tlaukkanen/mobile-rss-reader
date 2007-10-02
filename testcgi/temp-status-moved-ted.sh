36fa18e048f23ecc1bcf9bc5d7f8de3b58cbd2c0
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2007-10-02 22:59:15 +0000

    Test header redirection.

---
+++ testcgi/temp-status-moved-ted.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+printf "Status: 200 Moved Temporarily\n";
+printf "Location:http://rssreader.freewebpage.org/testdata/rss-ted-redirected-atom.xml\n\n";
---
+++ testcgi/temp-status-netvibes-opml-moved.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+printf "Status: 200 Moved Temporarily\n";
+printf "Location:http://rssreader.freewebpage.org/testdata/netvibes-subscriptions-opml.xml\n\n";
