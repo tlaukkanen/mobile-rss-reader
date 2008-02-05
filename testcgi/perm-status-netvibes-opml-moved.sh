f02643ec433642e3b32a993e7c2bfb624a281f8b
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2008-02-05 11:11:27 +0000

    Change URL to be for sourceforge test data.

--- testcgi/perm-status-netvibes-opml-moved.sh
+++ testcgi/perm-status-netvibes-opml-moved.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#Test data
 printf "Status: 301 Moved Permanantly\n";
-printf "Location:http://rssreader.freewebpage.org/testdata/netvibes-subscriptions-opml.xml\n\n";
+printf "Location:http://mobilerssreader.sourceforge.net/testdata/netvibes-subscriptions-opml.xml\n\n";
