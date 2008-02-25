90f54273e9d3ff984146183bc84674727698294c
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2008-02-25 08:52:12 +0000

    Move copyright to the top.

--- src/com/substanceofcode/rssreader/businesslogic/Controller.java
+++ src/com/substanceofcode/rssreader/businesslogic/Controller.java
@@ -19,7 +19,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-
 package com.substanceofcode.rssreader.businesslogic;
 
 import com.substanceofcode.rssreader.presentation.RssReaderMIDlet;
