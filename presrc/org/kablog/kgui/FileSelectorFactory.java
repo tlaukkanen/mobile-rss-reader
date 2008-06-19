fec1e4c1ddfc8bfdf5c2637559f76c751bde0844
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2008-06-19 02:48:18 +0000

    Specify approximate modify date as GPL software that is modified must contain the first date modified.

--- presrc/org/kablog/kgui/FileSelectorFactory.java
+++ presrc/org/kablog/kgui/FileSelectorFactory.java
@@ -18,7 +18,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE. 
  * 
- * 
+ * This software was originally modified no later than Sept 25, 2007.
  */
 
 // Expand to define DJSR75 define
