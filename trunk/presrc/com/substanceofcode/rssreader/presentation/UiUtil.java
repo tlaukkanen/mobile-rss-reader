2d241e98d758971b5dc9dcf64ee0243061aab60f
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2008-07-16 03:22:36 +0000

    Have creat image in UiUtil.  Put insert/add/append code in UiUtil.  Make methods final for minor performance.  Delete items from form utility.  Utilities to store/restore items.  Code to get the selected index and default to 0 if nothing selected.  Utility to create a choice, set layout to bottom and append to the form.  Add standard commands to share them.

--- trunk/presrc/com/substanceofcode/rssreader/presentation/UiUtil.java
+++ trunk/presrc/com/substanceofcode/rssreader/presentation/UiUtil.java
@@ -33,10 +33,24 @@
 
 package com.substanceofcode.rssreader.presentation;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.Item;
 import javax.microedition.lcdui.Gauge;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
 // If not using the test UI define the J2ME UI's
 //#ifndef DTESTUI
 import javax.microedition.lcdui.ChoiceGroup;
@@ -71,13 +85,14 @@ import net.sf.jlogmicro.util.logging.Level;
  * @author  Irving Bunton
  * @version 1.0
  */
-public class UiUtil {
+final public class UiUtil {
  
-	//#ifdef DLOGGING
-    private javax.microedition.lcdui.Form m_debug;
-    private Logger logger;
-	//#endif
-    
+	private static Command m_backCmd;
+	private static Command m_cancelCmd;
+	private static Command m_exitCmd;
+	private static Command m_helpCmd;
+	private static Command m_okCmd;
+	private static Command m_stopCmd;
   /**
    * Create a new command using the resource key and standard parms
    *
@@ -87,7 +102,8 @@ public class UiUtil {
    * @return    Command
    * @author Irv Bunton
    */
-    public static Command getCmdRsc(String key, int commandType, int priority) {
+    final public static Command getCmdRsc(String key, int commandType,
+			int priority) {
 		//#ifdef DLOGGING
 		Logger logger = Logger.getLogger("UiUtil");
 		logger.finest("key,commandType,priority=" + key + "," + commandType + "," + priority);
@@ -105,7 +121,7 @@ public class UiUtil {
    * @return    Command
    * @author Irv Bunton
    */
-    public static Command getCmdRsc(String key, String lngkey,
+    final public static Command getCmdRsc(String key, String lngkey,
 			int commandType, int priority) {
 		//#ifdef DLOGGING
 		Logger logger = Logger.getLogger("UiUtil");
@@ -130,10 +146,385 @@ public class UiUtil {
    * @param cmds
    * @author Irv Bunton
    */
-	public static void delCmds(Displayable disp, Command[] cmds) {
+	final public static void delCmds(Displayable disp, Command[] cmds) {
 		for (int ic = 0; ic < cmds.length; ic++) {
 			disp.removeCommand( cmds[ic] );
 		}
 	}
 
+  /**
+   * Delete items from the form.
+   *
+   * @param aForm
+   * @author Irv Bunton
+   */
+	//#ifdef DTESTUI
+	final public static void delItems(javax.microedition.lcdui.Form aForm)
+	//#else
+	final public static void delItems(Form aForm)
+	//#endif
+	{
+		//#ifdef DMIDP20
+		if(aForm.size()>0) {
+			aForm.deleteAll();
+		}
+		//#else
+		int fsize = aForm.size();
+		while(fsize-- > 0) {
+			aForm.delete(0);
+		}
+		//#endif
+	}
+
+  /**
+   * Delete items from the list.
+   *
+   * @param aList
+   * @author Irv Bunton
+   */
+	final public static void delItems(List aList) {
+		//#ifdef DMIDP20
+		if(aList.size()>0) {
+			aList.deleteAll();
+		}
+		//#else
+		int lsize = aList.size();
+		while(lsize-- > 0) {
+			aList.delete(0);
+		}
+		//#endif
+	}
+
+  /**
+   * Get the place (index) in a list to insert/append an element if using
+   * an inert, add, or append command.
+   *
+   * @param c - command selected by user
+   * @param cplace - current place selected in list (-1 if no selection)
+   * @param insCmd
+   * @param addCmd
+   * @param appndCmd
+   * @param plist
+   * @return    final
+   * @author Irv Bunton
+   */
+	final static int getPlaceIndex(Command c, int cplace, Command insCmd,
+							Command addCmd,
+							Command appndCmd,
+							javax.microedition.lcdui.List plist) {
+		if( (c == insCmd ) || (c == addCmd ) || (c == appndCmd )) {
+			final int blen = plist.size();
+			int addElem = (cplace == -1) ? blen : cplace;
+			if( c == addCmd ){
+				if (addElem < blen) {
+					addElem++;
+				}
+			}
+			if (c == appndCmd ) {
+				addElem = blen;
+			}
+			if ((addElem < 0) || (addElem > blen)) {
+				addElem = blen;
+			}
+			return addElem;
+		} else {
+			return -1;
+		}
+	}
+
+	/* Restore previous values. */
+	final static public void restorePrevValues(Item[] items, byte[] bdata) {
+		//#ifdef DLOGGING
+		Logger logger = Logger.getLogger("UiUtil");
+		boolean finestLoggable = logger.isLoggable(Level.FINEST);
+		//#endif
+		DataInputStream dis = new DataInputStream(
+				new ByteArrayInputStream(bdata));
+		for (int ic = 0; ic < items.length; ic++) {
+			try {
+				final Item item = items[ic];
+				if (item instanceof ChoiceGroup) {
+					((ChoiceGroup)item).setSelectedIndex(dis.readInt(),
+						true);
+					//#ifdef DLOGGING
+					if (finestLoggable) {logger.finest("set selected " + ((ChoiceGroup)item).getSelectedIndex());}
+					//#endif
+				} else if (item instanceof TextField) {
+					final int len = dis.readInt();
+					byte [] bvalue = new byte[len];
+					dis.read(bvalue);
+					String value;
+					try {
+						value = new String(bvalue, "UTF-8");
+					} catch (UnsupportedEncodingException e) {
+						value = new String(bvalue);
+						//#ifdef DLOGGING
+						logger.severe("cannot convert value=" + value, e);
+						//#endif
+					}
+					((TextField)item).setString(value);
+					//#ifdef DLOGGING
+					if (finestLoggable) {logger.finest("set string " + ((TextField)item).getString());}
+					//#endif
+				}
+			} catch (IOException e) {
+				//#ifdef DLOGGING
+				logger.severe("IOException reading selected.", e);
+				//#endif
+			}
+		}
+		if (dis != null) {
+			try { dis.close(); } catch (IOException e) {}
+		}
+	}
+
+	/* Store current values. */
+	final public static byte[] storeValues(Item[] items) {
+		//#ifdef DLOGGING
+		Logger logger = Logger.getLogger("UiUtil");
+		boolean finestLoggable = logger.isLoggable(Level.FINEST);
+		//#endif
+		ByteArrayOutputStream bout = new
+				ByteArrayOutputStream();
+		DataOutputStream dout = new
+				DataOutputStream( bout );
+		for (int ic = 0; ic < items.length; ic++) {
+			try {
+				final Item item = items[ic];
+				if (item instanceof ChoiceGroup) {
+					dout.writeInt(((ChoiceGroup)item).getSelectedIndex());
+					//#ifdef DLOGGING
+					if (finestLoggable) {logger.finest("stored selected " + item.getLabel() + "," + ((ChoiceGroup)item).getSelectedIndex());}
+					//#endif
+				} else if (item instanceof TextField) {
+					final String value = ((TextField)item).getString();
+					dout.writeInt(value.length());
+					byte [] bvalue;
+					try {
+						bvalue = value.getBytes("UTF-8");
+					} catch (UnsupportedEncodingException e) {
+						bvalue = value.getBytes();
+						//#ifdef DLOGGING
+						logger.severe("cannot store value=" + value, e);
+						//#endif
+					}
+					dout.write( bvalue, 0, bvalue.length );
+					//#ifdef DLOGGING
+					if (finestLoggable) {logger.finest("set string " + item.getLabel() + "," + ((TextField)item).getString());}
+					//#endif
+				}
+			} catch (IOException e) {
+				//#ifdef DLOGGING
+				logger.severe("IOException storing selected.", e);
+				//#endif
+			}
+		}
+		//#ifdef DLOGGING
+		if (finestLoggable) {logger.finest("bout.toByteArray().length=" + bout.toByteArray().length);}
+		//#endif
+		if (dout != null) {
+			try { dout.close(); } catch (IOException e) {}
+		}
+		return bout.toByteArray();
+	}
+
+  /**
+   * Return the selected index of the choice.  If nothing selected (-1),
+   * return 0 if size &gt; 0, or -1 if 0 size.
+   *
+   * Constructor
+   * @param choice - Choice interface
+   *
+   * @author Irv Bunton
+   */
+	final static public int getSelectedIndex(Choice choice) {
+		final int selIdx = choice.getSelectedIndex();
+		if (selIdx != -1) {
+			return selIdx;
+		} else {
+			if (choice.size() > 0) {
+				choice.setSelectedIndex(0, true);
+				return 0;
+			} else {
+				return -1;
+			}
+		}
+	}
+
+  /**
+   * Get the image for the image path.
+   *
+   * @param imagePath - Path for image
+   * @return    Image - image for path
+   *
+   * @author Irv Bunton
+   */
+	final public static Image getImage(final String imagePath) {
+		Image image = null;
+		try {
+			try {
+				// createImage("/icons/unread.png") does not always work
+				// with the emulator.  so, I do an alternate which is
+				// effectively the same thing.
+				image = Image.createImage(imagePath);
+			} catch(IOException e) {
+				//#ifdef DMIDP20
+				InputStream is =
+						imagePath.getClass().getResourceAsStream(imagePath);
+				if (is == null) {
+					throw new IOException("Cannot open resource " + imagePath);
+				}
+				image = Image.createImage(is);
+				is.close();
+				//#endif
+				//#ifdef DLOGGING
+				Logger logger = Logger.getLogger("UiUtil");
+				logger.warning(
+						"Could not get icon, alternate worked icons ex: ", e);
+				//#endif
+			}
+		} catch(Exception e) {
+			//#ifdef DLOGGING
+			Logger logger = Logger.getLogger("UiUtil");
+			logger.severe("Could not get image " + imagePath, e);
+			//#endif
+			System.err.println("Error while getting mark image: " + e.toString());
+		} finally {
+			return image;
+		}
+		
+	}
+
+	public static void showAlert(MIDlet midlet, Alert alert,
+			Displayable disp) {
+		//#ifdef DLOGGING
+		Logger logger = Logger.getLogger("UiUtil");
+		logger.finest("alert,disp=" + alert.getString() + "," + alert.getType());
+		//#endif
+		if (alert.getType() == AlertType.CONFIRMATION) {
+			//#ifdef DLOGGING
+			logger.finest("Alert not displayed when type is CONFIRMATION, changing to INFO");
+			//#endif
+			alert.setType(AlertType.INFO);
+		}
+		Display.getDisplay(midlet).setCurrent(alert, disp);
+	}
+
+  /**
+   * Create a ChoiceGroup, set the layout and add it to the form.
+   *
+   * @param label
+   * @param choices
+   * @return    ChoiceGroup
+   * @author Irv Bunton
+   */
+	static public ChoiceGroup getAddChoiceGroup(Form form, String label,
+												String[] choices) {
+        ChoiceGroup choiceGroup = new ChoiceGroup(label,
+				                            Choice.EXCLUSIVE, choices, null);
+		//#ifdef DMIDP20
+		choiceGroup.setLayout(Item.LAYOUT_BOTTOM);
+		//#endif
+        form.append( choiceGroup );
+		return choiceGroup;
+	}
+
+	static public void addStdCommands(final Displayable disp,
+									  final int[] types, 
+									  final String[] prompts, 
+									  final int[] priorities) {
+		for (int ic = 0; ic < types.length; ic++) {
+			Command cmd;
+			switch (types[ic]) {
+				case Command.BACK:
+					if ((disp instanceof Alert) ||
+							((m_backCmd != null) &&
+							 (priorities[ic] != m_backCmd.getPriority()))) {
+						cmd = UiUtil.getCmdRsc("cmd.back", types[ic],
+								priorities[ic]);
+					} else if (m_backCmd == null) {
+						cmd =
+						m_backCmd = UiUtil.getCmdRsc("cmd.back", types[ic],
+								priorities[ic]);
+					} else {
+						cmd = m_backCmd;
+					}
+					break;
+				case Command.CANCEL:
+					if ((disp instanceof Alert) ||
+							((m_cancelCmd != null) &&
+							 (priorities[ic] != m_cancelCmd.getPriority()))) {
+						cmd = UiUtil.getCmdRsc("cmd.cancel", types[ic],
+								priorities[ic]);
+					} else if (m_cancelCmd == null) {
+						cmd =
+						m_cancelCmd = UiUtil.getCmdRsc("cmd.cancel", types[ic],
+								priorities[ic]);
+					} else {
+						cmd = m_cancelCmd;
+					}
+					break;
+				case Command.EXIT:
+					if ((disp instanceof Alert) ||
+							((m_exitCmd != null) &&
+							 (priorities[ic] != m_exitCmd.getPriority()))) {
+						cmd = UiUtil.getCmdRsc("cmd.exit", types[ic],
+								priorities[ic]);
+					} else if (m_exitCmd == null) {
+						cmd =
+						m_exitCmd = UiUtil.getCmdRsc("cmd.exit", types[ic],
+								priorities[ic]);
+					} else {
+						cmd = m_exitCmd;
+					}
+					break;
+				case Command.HELP:
+					if ((disp instanceof Alert) ||
+							((m_helpCmd != null) &&
+							 (priorities[ic] != m_helpCmd.getPriority()))) {
+						cmd = UiUtil.getCmdRsc("cmd.help", types[ic],
+								priorities[ic]);
+					} else if (m_helpCmd == null) {
+						cmd =
+						m_helpCmd = UiUtil.getCmdRsc("cmd.help", types[ic],
+								priorities[ic]);
+					} else {
+						cmd = m_helpCmd;
+					}
+					break;
+				case Command.OK:
+					if ((disp instanceof Alert) ||
+							((m_okCmd != null) &&
+							 (priorities[ic] != m_okCmd.getPriority()))) {
+						cmd = UiUtil.getCmdRsc("cmd.ok", types[ic],
+								priorities[ic]);
+					} else if (m_okCmd == null) {
+						cmd =
+						m_okCmd = UiUtil.getCmdRsc("cmd.ok", types[ic],
+								priorities[ic]);
+					} else {
+						cmd = m_okCmd;
+					}
+					break;
+				case Command.STOP:
+					if ((disp instanceof Alert) ||
+							((m_stopCmd != null) &&
+							 (priorities[ic] != m_stopCmd.getPriority()))) {
+						cmd = UiUtil.getCmdRsc("cmd.stop", types[ic],
+								priorities[ic]);
+					} else if (m_stopCmd == null) {
+						cmd =
+						m_stopCmd = UiUtil.getCmdRsc("cmd.stop", types[ic],
+								priorities[ic]);
+					} else {
+						cmd = m_stopCmd;
+					}
+					break;
+				default:
+					throw new IllegalArgumentException("Command type " +
+							types[ic] + " is not valid.");
+			}
+			disp.addCommand(cmd);
+		}
+	}
 }
