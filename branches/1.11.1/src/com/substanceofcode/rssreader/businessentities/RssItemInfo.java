5c83b5c095a64e5079073d21695fa8f41235f33c
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2009-09-22 07:31:51 +0000

    Simplify/fix compatibility tests.

---
+++ branches/1.11.1/src/com/substanceofcode/rssreader/businessentities/RssItemInfo.java
@@ -0,0 +1,95 @@
+/*
+ * RssItemInfo.java
+ *
+ * Copyright (C) 2005-2006 Tommi Laukkanen
+ * http://www.substanceofcode.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+// Expand to define logging define
+//#define DNOLOGGING
+// Expand to define test define
+//#define DNOTEST
+//#ifdef DTEST
+//@package com.substanceofcode.rssreader.businessentities;
+//@
+//@import com.substanceofcode.utils.Base64;
+//@import com.substanceofcode.utils.StringUtil;
+//@import java.io.UnsupportedEncodingException;
+//@import java.util.Date;
+//@
+//@/**
+//@ * RssItemInfo class is a data store for a single item in RSS feed.
+//@ * One item consist of title, link, description and optional date.
+//@ *
+//@ * @author  Tommi Laukkanen
+//@ * @version 1.1
+//@ */
+//@public interface RssItemInfo {
+//@    
+//@    /** Get RSS item title */
+//@    String getTitle();
+//@    
+//@    /** Get RSS item link address */
+//@    String getLink();
+//@    
+//@    void setLink(String link);
+//@
+//@    /** Get RSS item description */
+//@    String getDescription();
+//@    
+//@    /** Get RSS item description */
+//@    void setDescription(String desc);
+//@    
+//@    /** Get RSS item publication date */
+//@    Date getDate();
+//@    
+//@    void setDate(Date date);
+//@
+//@    /** Serialize the object
+//@	  When we serialize we don't do anything special for itunes as the
+//@	  store to memory will be deserialized only by the iTunes capable
+//@	  version.
+//@	  */
+//@    String unencodedSerialize();
+//@
+//@    /** Serialize the object
+//@	  this serialize does not need to know if Itunes is capable/enabled given
+//@	  that no fields were added to make it capable/enabled
+//@	  */
+//@    String serialize();
+//@		
+//@	/* Copy to item. */
+//@	/* UNDO?
+//@	RssItemInfo copyTo(RssItemInfo item);
+//@	*/
+//@
+	//#ifdef DTEST
+//@	/* Compare item. */
+//@	boolean equals(RssItemInfo item);
+	//#endif
+//@
+//@    void setUnreadItem(boolean unreadItem);
+//@
+//@    boolean isUnreadItem();
+//@
+//@    void setEnclosure(String enclosure);
+//@
+//@    String getEnclosure();
+//@
+//@}
+//#endif
