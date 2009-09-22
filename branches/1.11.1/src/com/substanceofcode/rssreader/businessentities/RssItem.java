0eeece861c88e6ca6024acc98b18002ae196b125
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2009-09-22 13:32:00 +0000

    Use/write JMUnit tests.

--- branches/1.11.1/src/com/substanceofcode/rssreader/businessentities/RssItem.java
+++ branches/1.11.1/src/com/substanceofcode/rssreader/businessentities/RssItem.java
@@ -27,7 +27,7 @@
 // Expand to define test ui define
 //#define DNOTESTUI
 // Expand to define JMUnit test define
-//#define ${DJMTESTDEF}
+//#define DNOJMTEST
 //#ifdef DTESTUI
 //#define HAS_EQUALS
 //#endif
