3a35eba45db9b6fb95e2616a4545f512ee3d42c5
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-07-05 19:10:57 +0000

    Update source text to make distribution/redistribution license options clearer.

--- presrc/net/yinlight/j2me/observable/ObservableHandler.java
+++ presrc/net/yinlight/j2me/observable/ObservableHandler.java
@@ -8,7 +8,7 @@
  * it under the terms the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * You can redistribute it and/or modify
+ * Alternately, at your option, you can redistribute the software and/or modify
  * it under the terms the GNU Lessor General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
@@ -18,14 +18,16 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ * You should have received a copy of the GNU General Public License or
+ * GNU Lesser Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
 /*
  * IB 2010-06-15 1.11.5Dev2 Use observer pattern for feed parsing to prevent hangs from spotty networks and bad URLs.
  * IB 2010-06-15 1.11.5Dev2 Use version that can be distributed as GPL or LGPL.
+ * IB 2010-07-05 1.11.5Dev7 Update source text to make distribution/redistribution license options clearer.
  */
 
 // Expand to define MIDP define
