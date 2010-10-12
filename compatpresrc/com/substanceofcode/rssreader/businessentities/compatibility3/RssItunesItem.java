3bfe42bbc73b4e8dfed27abf69840de5ca07c01d
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 10:12:18 +0000

    Add //whyaddpreprocess for RIM preprocessor.  Change to --Need to modify--whyaddpreprocess to modify to become //whyaddpreprocess for RIM preprocessor.

--- compatpresrc/com/substanceofcode/rssreader/businessentities/compatibility3/RssItunesItem.java
+++ compatpresrc/com/substanceofcode/rssreader/businessentities/compatibility3/RssItunesItem.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * RssItunesItem.java
  *
@@ -23,6 +24,8 @@
  */
 /*
  * IB 2010-04-17 1.11.5RC2 Change to put compatibility classes in compatibility packages.
+ * IB 2010-09-29 1.11.5Dev8 Add //#preprocess for RIM preprocessor.
+ * IB 2010-10-12 1.11.5Dev9 Change to --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  */
 
 // Expand to define logging define
