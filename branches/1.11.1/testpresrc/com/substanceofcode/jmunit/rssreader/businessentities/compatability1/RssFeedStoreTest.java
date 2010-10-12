da21055e5b6de57cdaf400ea41fe2d3dd5c60a93
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 10:20:46 +0000

    Add --Need to modify--whyaddpreprocess to modify to become //whyaddpreprocess for RIM preprocessor.

--- branches/1.11.1/testpresrc/com/substanceofcode/jmunit/rssreader/businessentities/compatability1/RssFeedStoreTest.java
+++ branches/1.11.1/testpresrc/com/substanceofcode/jmunit/rssreader/businessentities/compatability1/RssFeedStoreTest.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * RssFeedStoreTest.java
  *
@@ -22,6 +23,7 @@
 /*
  * IB 2010-03-28 1.11.5RC2 Updated date defaults to "".
  * IB 2010-04-17 1.11.5RC2 Change to put compatibility classes in compatibility packages.
+ * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  */
 
 // Expand to define test define
