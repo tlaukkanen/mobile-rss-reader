5d5a6c7e871ba322edcefc36ab46c99c1f2a267c
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 11:03:46 +0000

    Add --Need to modify--/home/irvb/scmworkspace/j2merss1opreprocess to modify to become ///home/irvb/scmworkspace/j2merss1opreprocess for RIM preprocessor.

--- branches/1.11.1/testpresrc/com/substanceofcode/jmunit/rssreader/businessentities/RssFeedSuite.java
+++ branches/1.11.1/testpresrc/com/substanceofcode/jmunit/rssreader/businessentities/RssFeedSuite.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * RssFeedSuite.java
  *
@@ -21,6 +22,7 @@
  */
 /*
  * IB 2010-05-24 1.11.5RC2 Move compatibility tests to compatibility packages.
+ * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  */
 
 // Expand to define test define
