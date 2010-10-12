20b4d355999efb4e40851f21d665497e3eafd384
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 11:00:57 +0000

    Add --Need to modify--/home/irvb/scmworkspace/j2merss1opreprocess to modify to become ///home/irvb/scmworkspace/j2merss1opreprocess for RIM preprocessor.

--- branches/1.11.1/presrc/com/substanceofcode/rssreader/presentation/RssReaderMIDlet.java
+++ branches/1.11.1/presrc/com/substanceofcode/rssreader/presentation/RssReaderMIDlet.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * RssReaderMIDlet.java
  *
@@ -86,6 +87,7 @@
  * IB 2010-09-27 1.11.5Dev8 Use setCurrentFeature to go back to list before item form.
  * IB 2010-09-27 1.11.5Dev8 Have loadForm for procUpdHeader.
  * IB 2010-09-27 1.11.5Dev8 Have loadForm for procBackHeader.
+ * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
 */
 
 // Expand to define test define
