d32a3ab93c946f5de117ee28fb4f20393ee4d828
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 10:19:07 +0000

    Change to --Need to modify--whyaddpreprocess to modify to become //whyaddpreprocess for RIM preprocessor.

--- branches/1.11.1/src/com/substanceofcode/utils/EncodingStreamReader.java
+++ branches/1.11.1/src/com/substanceofcode/utils/EncodingStreamReader.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * EncodingStreamReader.java
  *
@@ -21,6 +22,7 @@
  */
 /*
  * IB 2010-07-04 1.11.5Dev6 Don't use m_ prefix for parameter definitions.
+ * IB 2010-10-12 1.11.5Dev9 Change to --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  */
 
 // Expand to define testing define
