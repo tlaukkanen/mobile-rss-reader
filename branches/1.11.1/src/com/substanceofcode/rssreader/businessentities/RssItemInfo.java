e0c99228c416533dd7cfcb0b099aa9a2065ca41c
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 11:01:20 +0000

    Add --Need to modify--/home/irvb/scmworkspace/j2merss1opreprocess to modify to become ///home/irvb/scmworkspace/j2merss1opreprocess for RIM preprocessor.

--- branches/1.11.1/src/com/substanceofcode/rssreader/businessentities/RssItemInfo.java
+++ branches/1.11.1/src/com/substanceofcode/rssreader/businessentities/RssItemInfo.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * RssItemInfo.java
  *
@@ -23,6 +24,7 @@
  * IB 2010-03-07 1.11.4RC1 Use feed interface only for testing.
  * IB 2010-03-07 1.11.4RC1 Code cleanup.
  * IB 2010-03-07 1.11.4RC1 Need toString.
+ * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
 */
 
 // Expand to define logging define
