44154cc7fa8388b2df6089ca585c340f6c80cfee
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 10:14:28 +0000

    Add //whyaddpreprocess for RIM preprocessor.

--- branches/1.11.1/compatpresrc/com/substanceofcode/rssreader/businesslogic/compatibility4/LineByLineParser.java
+++ branches/1.11.1/compatpresrc/com/substanceofcode/rssreader/businesslogic/compatibility4/LineByLineParser.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * LineByLineParser.java
  *
@@ -22,6 +23,8 @@
 /*
  * IB 2010-05-24 1.11.5RC2 Test compatibility LineByLineParser.
  * IB 2010-05-24 1.11.5RC2 Put compatibility4 in getLogger.
+ * IB 2010-09-29 1.11.5Dev8 Add //#preprocess for RIM preprocessor.
+ * IB 2010-10-12 1.11.5Dev9 Change to --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  */
 
 // Expand to define logging define
