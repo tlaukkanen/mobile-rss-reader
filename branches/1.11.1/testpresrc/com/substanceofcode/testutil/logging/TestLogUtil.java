90db74f88cd74db2ea3c9f2af66dfde245a8b2bc
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 10:23:25 +0000

    Add --Need to modify--whyaddpreprocess to modify to become //whyaddpreprocess for RIM preprocessor.

--- branches/1.11.1/testpresrc/com/substanceofcode/testutil/logging/TestLogUtil.java
+++ branches/1.11.1/testpresrc/com/substanceofcode/testutil/logging/TestLogUtil.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  *
  * Copyright (C) 2008 Irving Bunton
@@ -21,6 +22,7 @@
 /*
  * IB 2010-05-24 1.11.5RC2 Code cleanup.
  * IB 2010-05-24 1.11.5RC2 Better logging.
+ * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  */
 
 // Expand to define JMUnit test define
