f8cc1bd9afeb012e3547d132e6642df00f8a3d94
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-10-12 11:03:52 +0000

    Add --Need to modify--/home/irvb/scmworkspace/j2merss1opreprocess to modify to become ///home/irvb/scmworkspace/j2merss1opreprocess for RIM preprocessor.

--- testpresrc/com/substanceofcode/jmunit/rssreader/businesslogic/ParserSuite.java
+++ testpresrc/com/substanceofcode/jmunit/rssreader/businesslogic/ParserSuite.java
@@ -1,3 +1,4 @@
+//--Need to modify--#preprocess
 /*
  * ParserSuite.java
  *
@@ -21,6 +22,7 @@
  */
 /*
  * IB 2010-05-24 1.11.5RC2 Unit test RssFeedParserSuite and FeedListParserSuite.
+ * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  */
 
 // Expand to define test define
