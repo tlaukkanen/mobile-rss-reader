6b5c43e9567f425eff03fc5aeb2e2a4ede4d60d3
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2010-11-19 16:31:23 +0000

    Allow an object to be written to with the find files result using FeatureMgr.setTxtObj.  Allow title for find files as a parameter to allow more generic doLaunchSelector in KFileSelectorMgr.  Have println in DTEST.  Have debug conditional println use displayDbgMsg.  Have displayDbgMsg and be in DTEST.  Change debug log info.

--- src/org/kablog/kgui/KFileSelectorMgr.java
+++ src/org/kablog/kgui/KFileSelectorMgr.java
@@ -18,14 +18,14 @@
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE. 
- * 
- * 
- * 
+ * IN THE SOFTWARE.
+ *
+ *
+ *
  * This software was originally modified no later than Sept 25, 2007.
  */
 /*
- * IB 2010-03-21 1.11.5RC1 Remove KFileSelector to save memory. 
+ * IB 2010-03-21 1.11.5RC1 Remove KFileSelector to save memory.
  * IB 2010-04-30 1.11.5RC2 Track threads used.
  * IB 2010-07-04 1.11.5Dev6 Use null pattern for nulls to initialize and save memory.
  * IB 2010-08-15 1.11.5Dev8 Remove unused reqSetVisible.
@@ -37,6 +37,12 @@
  * IB 2010-09-26 1.11.5Dev8 Use setCurrentItemFeature instead of setCurrentItemMgr.
  * IB 2010-09-27 1.11.5Dev8 Move title to construtor for KFileSelectorImpl.
  * IB 2010-10-12 1.11.5Dev8 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
+ * IB 2010-11-18 1.11.5Dev14 Allow an object to be written to with the find files result using FeatureMgr.setTxtObj.
+ * IB 2010-11-19 1.11.5Dev14 Allow title for find files as a parameter to allow more generic doLaunchSelector in KFileSelectorMgr.
+ * IB 2010-11-19 1.11.5Dev14 Have println in DTEST.
+ * IB 2010-11-19 1.11.5Dev14 Have debug conditional println use displayDbgMsg.
+ * IB 2010-11-19 1.11.5Dev14 Have displayDbgMsg and be in DTEST.
+ * IB 2010-11-19 1.11.5Dev14 Change debug log info.
 */
 
 // Expand to define MIDP define
@@ -55,6 +61,7 @@
 //@import com.substanceofcode.rssreader.presentation.LoadingForm;
 //@import com.substanceofcode.rssreader.presentation.FeatureForm;
 //@import com.substanceofcode.utils.MiscUtil;
+//@import com.substanceofcode.rssreader.presentation.FeatureMgr;
 //@
 //#ifdef DLOGGING
 //@import net.sf.jlogmicro.util.logging.Logger;
@@ -69,13 +76,13 @@
 //@ * @author  Todd C. Stellanova
 //@ */
 //@public class KFileSelectorMgr
-//@implements KViewParent 
+//@implements KViewParent
 //@{
 //@
 //@	final       Object nullPtr = null;
-//@	protected FeatureForm txtFrm;
-//@	protected TextField txtFld;
-//@	protected KFileSelectorImpl fileSelectorView; 
+//@	protected Displayable txtDisp;
+//@	protected Object txtObj;
+//@	protected KFileSelectorImpl fileSelectorView;
 //@    protected KViewParent viewParent;
 //@    protected boolean ready = false;
 //@    protected boolean bDebug;
@@ -90,42 +97,45 @@
 //@     * When the we're is done capturing an XML or multi-media, it calls this
 //@	   method.
 //@     */
-//@    final public void childFinished(KViewChild child) {   
+//@    final public void childFinished(KViewChild child) {
 //@		try {
 //@			if (fileSelectorView.getSelectedURL() != null) {
-//@				txtFld.setString(fileSelectorView.getSelectedURL());
+//@				FeatureMgr.setTxtObj(txtObj, fileSelectorView.getSelectedURL());
 //@			}
 //@			fileSelectorView.doCleanup();
+//@			FeatureMgr.setCurrentObjMgr( fileSelectorView.getFeatureMgr(),
+//@					txtObj, txtDisp);
 //@			// Save memory
 //@			fileSelectorView = (KFileSelectorImpl)nullPtr;
-//@			fileSelectorView.getFeatureMgr().setCurrentItemFeature(txtFrm,
-//@					txtFld, txtFrm);
 //@		} catch (Throwable t) {
 			//#ifdef DLOGGING
 //@			logger.severe("Sort dates error.", t);
 			//#endif
+			//#ifdef DTEST
 //@			System.out.println("Sort dates error." + t + " " +
 //@							   t.getMessage());
+			//#endif
 //@			t.printStackTrace();
 //@		}
 //@	}
-//@        
+//@
 //@	/* Start the file selector list. */
 //@	final public void doLaunchSelector(
-//@			boolean selectDir, FeatureForm txtFrm, TextField txtFld,
+//@			boolean selectDir, String findTitle,
+//@			Displayable txtDisp, Object txtObj,
 //@			LoadingForm loadForm)
 //@	throws Throwable {
 //@
+		//#ifdef DTEST
 //@		System.out.println("doLaunchSelector...");
-//@		this.txtFrm = txtFrm;
-//@		this.txtFld = txtFld;
+		//#endif
+//@		this.txtDisp = txtDisp;
+//@		this.txtObj = txtObj;
 //@
 //@		fileSelectorView = (KFileSelectorImpl)nullPtr;
 //@
 //@		try {
-//@			fileSelectorView = new KFileSelectorImpl(
-//@					((txtFrm instanceof ImportFeedsForm) ? "Find import file" :
-//@					 "Find feed file"), loadForm);
+//@			fileSelectorView = new KFileSelectorImpl(findTitle, loadForm);
 //@			fileSelectorView.init(selectDir, null, "/icons" );
 //@			fileSelectorView.setCommandListener(fileSelectorView, true);
 //@			fileSelectorView.setViewParent(this);
@@ -134,7 +144,9 @@
 //@		}
 //@		catch (Throwable e)
 //@		{
-//@			if (bDebug) System.out.println("Go to find files fail: " + e);
+			//#ifdef DTEST
+//@			displayDbgMsg("Go to find files fail: " + e, null);
+			//#endif
 			//#ifdef DLOGGING
 //@			logger.severe("Go to find files fail: ", e);
 			//#endif
@@ -147,21 +159,25 @@
 //@	/** We've updated the child's status.
 //@	 */
 //@	final public void childStatusChanged(KViewChild child, int statusType, int status) {
-//@		if (bDebug) System.out.println("Child status changed: " + status);
-//@	} 
+		//#ifdef DTEST
+//@		displayDbgMsg("Child status changed: " + status, null);
+		//#endif
+//@	}
 //@
 //@    /** @param The callback client interested in receiving finished status.
 //@     */
 //@    final public void setViewParent(KViewParent parent) {
 //@        this.viewParent = parent;
 //@    }
-//@    
-//@    /** 
+//@
+	//#ifdef DTEST
+//@    /**
 //@     Display a debug message somehow
 //@     */
 //@    final public void displayDbgMsg(String msg, AlertType type) {
 //@         if (bDebug) System.out.println("dbgMsg: " + msg);
 //@	}
+	//#endif
 //@
 //@	/* Add a deferred action.  This is either passed on to our parent or
 //@	   run as a thread now. */
