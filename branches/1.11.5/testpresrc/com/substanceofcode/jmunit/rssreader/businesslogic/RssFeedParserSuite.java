2c505c79dc9bbadf1fb1b3d28d4166417ae52ba8
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-01-18 04:12:17 +0000

    Read changes in change log or do a diff and look at the comments at the top.

--- branches/1.11.5/testpresrc/com/substanceofcode/jmunit/rssreader/businesslogic/RssFeedParserSuite.java
+++ branches/1.11.5/testpresrc/com/substanceofcode/jmunit/rssreader/businesslogic/RssFeedParserSuite.java
@@ -23,8 +23,14 @@
 /*
  * IB 2010-03-14 1.11.5RC2 Test RSS feed parser.
  * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
+ * IB 2011-01-14 1.11.5Alpha15 Only compile this if it is the full version.
+ * IB 2011-01-14 1.11.5Alpha15 Use conditional preprocessed cldc11 code with modifications instead of cldc10 code.
  */
 
+// Expand to define full vers define
+@DFULLVERSDEF@
+// Expand to define full vers define
+@DINTLINKDEF@
 // Expand to define test define
 @DTESTDEF@
 // Expand to define JMUnit test define
@@ -33,11 +39,12 @@
 @DLOGDEF@
 
 //#ifdef DJMTEST
+//#ifdef DFULLVERS
 package com.substanceofcode.jmunit.rssreader.businesslogic;
 
 import com.substanceofcode.jmunit.rssreader.businesslogic.RssFeedParserTest;
 
-import jmunit.framework.cldc10.TestSuite;
+import jmunit.framework.cldc11.TestSuite;
 
 final public class RssFeedParserSuite extends TestSuite {
 
@@ -47,4 +54,5 @@ final public class RssFeedParserSuite extends TestSuite {
 	}
 
 }
+//#endif
 //#endif
