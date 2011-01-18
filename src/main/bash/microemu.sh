01f9307e036c749907b8c2b6b138c67d4ff835cb
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-01-18 04:11:13 +0000

    Read changes in change log or do a diff and look at the comments at the top.

--- src/main/bash/microemu.sh
+++ src/main/bash/microemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # IB IB 2010-11-17 1.11.5Dev14 Have microemu.sh to run microemulator demo of application version.
 export CLASSPATH="$CLASSPATH:\
-lib/@EMULATORJAR@:lib/@BUILDFILEROOT@.jar:lib/microemu-device-large@FILEVERSPREFIX@.jar:lib/midpapi20@FILEVERSPREFIX@.jar:lib/cldcapi11@FILEVERSPREFIX@.jar:lib/microemu-jsr-75@FILEVERSPREFIX@.jar"
+../signedlib/@EMULATORJAR@:../signedlib/@BUILDFILEROOT@.jar:../signedlib/@MICROEMUFNROOT@-device-large@FILEVERSPREFIX@.jar:../signedlib/@MICROEMUPREFIXSIGNED@midpapi20@FILEVERSPREFIX@.jar:../signedlib/@MICROEMUPREFIXSIGNED@cldcapi11@FILEVERSPREFIX@.jar:../signedlib/@MICROEMUFNROOT@-jsr-75@FILEVERSPREFIX@.jar"
 echo "CLASSPATH=$CLASSPATH"
 java @EMULATORCLASS@ @MIDLETCLASS@ --device org/microemu/device/large/device.xml
