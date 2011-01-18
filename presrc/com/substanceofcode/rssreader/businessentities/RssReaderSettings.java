223a74dd76d23cc3aecfed19dbfaf3f1311812f5
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-01-18 04:05:32 +0000

    Read changes in change log or do a diff and look at the comments at the top.

--- presrc/com/substanceofcode/rssreader/businessentities/RssReaderSettings.java
+++ presrc/com/substanceofcode/rssreader/businessentities/RssReaderSettings.java
@@ -33,6 +33,11 @@
  * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
  * IB 2010-11-16 1.11.5Dev14 Don't have feed open property now that back will have consistent usage.
  * IB 2010-11-16 1.11.5Dev14 Have back be 1, cancel be 2, stop be 3, ok be 4, open be 5, and select be 6.
+ * IB 2011-01-14 1.11.5Dev15 Have optional backlight after update/refresh all.
+ * IB 2011-01-14 1.11.5Dev15 Have optional vibrate after update/refresh all.
+ * IB 2011-01-14 1.11.5Dev15 Have some public static keys for int options to allow shorter coding in SettingsForm.
+ * IB 2011-01-14 1.11.5Dev15 Allow use text box option to be public when testing to test using it.
+ * IB 2011-01-14 1.11.5Dev15 Change static fields to instance vars for the singleton to reduce static memory used.
  */
 
 // Expand to define MIDP define
@@ -43,6 +48,8 @@
 @DITUNESDEF@
 // Expand to define test define
 @DTESTDEF@
+// Expand to define JMUnit test define
+@DJMTESTDEF@
 // Expand to define logging define
 @DLOGDEF@
 package com.substanceofcode.rssreader.businessentities;
@@ -69,22 +76,29 @@ final public class RssReaderSettings {
     volatile private Settings m_settings;
     volatile private static RssReaderSettings m_singleton = null;
 	//#ifdef DMIDP20
-    public static final int DEFAULT_FONT_CHOICE = 0;
+    public final int DEFAULT_FONT_CHOICE = 0;
 	//#endif
     
-    private static final String MAX_ITEM_COUNT = "max-item-count";
-    private static final String MAX_WORD_COUNT = "max-word-count";
-    private static final String IMPORT_URL = "import-url";
-    private static final String IMPORT_USERNAME = "import-username";
-    private static final String IMPORT_PASSWORD = "import-password";
-    private static final String MARK_UNREAD_ITEMS = "mark-unread-items";
-    private static final String ITUNES_ENABLED = "itunes-enabled";
+    public final String MAX_ITEM_COUNT = "max-item-count";
+    public final String MAX_WORD_COUNT = "max-word-count";
+    private final String IMPORT_URL = "import-url";
+    private final String IMPORT_USERNAME = "import-username";
+    private final String IMPORT_PASSWORD = "import-password";
+    private final String MARK_UNREAD_ITEMS = "mark-unread-items";
+    private final String ITUNES_ENABLED = "itunes-enabled";
 	//#ifdef DMIDP20
-    private static final String FONT_CHOICE = "font-choice";
-    private static final String FIT_POLICY = "fit-policy";
-    private static final String BOOKMARK_NAME_NEWS = "bookmark-name-news";
+    private final String FONT_CHOICE = "font-choice";
+    private final String FIT_POLICY = "fit-policy";
+    private final String BOOKMARK_NAME_NEWS = "bookmark-name-news";
+    public final String BACKLIGHT_FLASH_SECS = "backlight-flash-secs";
+    public final String VIBRATE_SECS = "vibrate-secs";
 	//#endif
-    private static final String USE_TEXT_BOX = "use-text-box";
+	//#ifdef DJMTEST
+    public
+	//#else
+    private
+	//#endif
+		static final String USE_TEXT_BOX = "use-text-box";
     private static final String USE_STANDARD_EXIT = "use-standard-exit";
     private static final String NOVICE = "novice";
     public static final int INIT_MAX_ITEM_COUNT = 10;
@@ -132,92 +146,92 @@ final public class RssReaderSettings {
     
     /** Get maximum item count */
     public int getMaximumItemCountInFeed() {
-        int maxCount = m_settings.getIntProperty(MAX_ITEM_COUNT,
+        int maxCount = m_settings.getIntProperty(m_singleton.MAX_ITEM_COUNT,
 				INIT_MAX_ITEM_COUNT);
         return maxCount;
     }
     
     /** Set maximum item count in feed */
     public void setMaximumItemCountInFeed(int maxCount) {
-        m_settings.setIntProperty(MAX_ITEM_COUNT, maxCount);
+        m_settings.setIntProperty(m_singleton.MAX_ITEM_COUNT, maxCount);
     }
     
     /** Get maximum word count in description */
     public int getMaxWordCountInDesc() {
-        int maxCount = m_settings.getIntProperty(MAX_WORD_COUNT, 10);
+        int maxCount = m_settings.getIntProperty(m_singleton.MAX_WORD_COUNT, 10);
         return maxCount;
     }
     
     /** Set maximum word count in description */
     public void setMaxWordCountInDesc(int maxCount) {
-        m_settings.setIntProperty(MAX_WORD_COUNT, maxCount);
+        m_settings.setIntProperty(m_singleton.MAX_WORD_COUNT, maxCount);
     }
     
     /** Get import URL address */
     public String getImportUrl() {
-        String url = m_settings.getStringProperty(0, IMPORT_URL, "");
+        String url = m_settings.getStringProperty(0, m_singleton.IMPORT_URL, "");
         return url;
     }
     
     /** Set import URL address */
     public void setImportUrl(String url) {
-        m_settings.setStringProperty( IMPORT_URL, url);
+        m_settings.setStringProperty(m_singleton.IMPORT_URL, url);
     }
     
     /** Get import URL username */
     public String getImportUrlUsername() {
-        String username = m_settings.getStringProperty(0, IMPORT_USERNAME, "");
+        String username = m_settings.getStringProperty(0, m_singleton.IMPORT_USERNAME, "");
         return username;
     }
     
     /** Set import URL username */
     public void setImportUrlUsername(String username) {
-        m_settings.setStringProperty( IMPORT_USERNAME, username);
+        m_settings.setStringProperty(m_singleton.IMPORT_USERNAME, username);
     }
     
     /** Get import URL password */
     public String getImportUrlPassword() {
-        String password = m_settings.getStringProperty(0, IMPORT_PASSWORD, "");
+        String password = m_settings.getStringProperty(0, m_singleton.IMPORT_PASSWORD, "");
         return password;
     }
     
     /** Set import URL password */
     public void setImportUrlPassword(String password) {
-        m_settings.setStringProperty( IMPORT_PASSWORD, password);
+        m_settings.setStringProperty(m_singleton.IMPORT_PASSWORD, password);
     }
     
     /** Get mark unread items */
     public boolean getMarkUnreadItems() {
 		//#ifdef DMIDP20
-        return m_settings.getBooleanProperty( MARK_UNREAD_ITEMS, true);
+        return m_settings.getBooleanProperty(m_singleton.MARK_UNREAD_ITEMS, true);
 		//#else
-        return m_settings.getBooleanProperty( MARK_UNREAD_ITEMS, false);
+        return m_settings.getBooleanProperty(m_singleton.MARK_UNREAD_ITEMS, false);
 		//#endif
     }
     
     /** Set import URL password */
     public void setMarkUnreadItems(boolean markUnreadItems) {
-        m_settings.setBooleanProperty( MARK_UNREAD_ITEMS, markUnreadItems);
+        m_settings.setBooleanProperty(m_singleton.MARK_UNREAD_ITEMS, markUnreadItems);
     }
     
     /** Get itunes enabled */
     public boolean getItunesEnabled() {
 		//#ifdef DITUNES
-        return m_settings.getBooleanProperty( ITUNES_ENABLED, true);
+        return m_settings.getBooleanProperty(m_singleton.ITUNES_ENABLED, true);
 		//#else
-        return m_settings.getBooleanProperty( ITUNES_ENABLED, false);
+        return m_settings.getBooleanProperty(m_singleton.ITUNES_ENABLED, false);
 		//#endif
     }
     
     /** Set feed list back is first command */
     public void setItunesEnabled(boolean itunesEnabled) {
-        m_settings.setBooleanProperty( ITUNES_ENABLED, itunesEnabled);
+        m_settings.setBooleanProperty(m_singleton.ITUNES_ENABLED, itunesEnabled);
     }
     
 	//#ifdef DMIDP20
     /** Get font choice */
     public int getFontChoice() {
-        return m_settings.getIntProperty( FONT_CHOICE, DEFAULT_FONT_CHOICE);
+        return m_settings.getIntProperty(m_singleton.FONT_CHOICE, DEFAULT_FONT_CHOICE);
     }
     
 	/* Get the font size. This is the actual size of the font */
@@ -243,76 +257,97 @@ final public class RssReaderSettings {
 
     /** Set font size */
     public void setFontChoice(int fontChoice) {
-        m_settings.setIntProperty( FONT_CHOICE, fontChoice);
+        m_settings.setIntProperty(m_singleton.FONT_CHOICE, fontChoice);
     }
     
     /** Get fit policy */
     public int getFitPolicy() {
-        return m_settings.getIntProperty( FIT_POLICY, List.TEXT_WRAP_DEFAULT);
+        return m_settings.getIntProperty(m_singleton.FIT_POLICY, List.TEXT_WRAP_DEFAULT);
     }
     
     /** Set fit policy */
     public void setFitPolicy(int fitPolicy) {
-        m_settings.setIntProperty( FIT_POLICY, fitPolicy);
+        m_settings.setIntProperty(m_singleton.FIT_POLICY, fitPolicy);
     }
     
     /** Get put bookmark name in news item list.*/
     public boolean getBookmarkNameNews() {
-        return m_settings.getBooleanProperty( BOOKMARK_NAME_NEWS, false);
+        return m_settings.getBooleanProperty(m_singleton.BOOKMARK_NAME_NEWS, false);
     }
     
     /** Set put bookmark name in news item list. */
     public void setBookmarkNameNews(boolean bookmarkNameNews) {
-        m_settings.setBooleanProperty( BOOKMARK_NAME_NEWS, bookmarkNameNews);
+        m_settings.setBooleanProperty(m_singleton.BOOKMARK_NAME_NEWS, bookmarkNameNews);
     }
     
     /** Get use text box */
     public boolean getUseTextBox() {
-        return m_settings.getBooleanProperty( USE_TEXT_BOX, false);
+        return m_settings.getBooleanProperty(m_singleton.USE_TEXT_BOX, false);
     }
     
     /** Set use text box */
     public void setUseTextBox(boolean useTextBox) {
-        m_settings.setBooleanProperty( USE_TEXT_BOX, useTextBox);
+        m_settings.setBooleanProperty(m_singleton.USE_TEXT_BOX, useTextBox);
     }
+
+    /** Get backlight flash secs */
+    public int getBacklightFlashSecs() {
+        return m_settings.getIntProperty(m_singleton.BACKLIGHT_FLASH_SECS, 0);
+    }
+    
+    /** Set backlight flash secs */
+    public void setBacklightFlashSecs(int backlightFlashSecs) {
+        m_settings.setIntProperty(m_singleton.BACKLIGHT_FLASH_SECS, backlightFlashSecs);
+    }
+    
+    /** Get vibrate secs */
+    public int getVibrateSecs() {
+        return m_settings.getIntProperty(m_singleton.VIBRATE_SECS, 0);
+    }
+    
+    /** Set vibrate secs */
+    public void setVibrateSecs(int vibrateSecs) {
+        m_settings.setIntProperty(m_singleton.VIBRATE_SECS, vibrateSecs);
+    }
+    
 	//#endif
     
     /** Get use standard exit */
     public boolean getUseStandardExit() {
-        return m_settings.getBooleanProperty( USE_STANDARD_EXIT, false);
+        return m_settings.getBooleanProperty(m_singleton.USE_STANDARD_EXIT, false);
     }
     
     /** Set standard exit */
     public void setUseStandardExit(boolean useStandardExit) {
-        m_settings.setBooleanProperty( USE_STANDARD_EXIT, useStandardExit);
+        m_settings.setBooleanProperty(m_singleton.USE_STANDARD_EXIT, useStandardExit);
     }
     
     /** Get novice user */
     public boolean getNovice() {
-        return m_settings.getBooleanProperty( NOVICE, false);
+        return m_settings.getBooleanProperty(m_singleton.NOVICE, false);
     }
     
     /** Set novice user */
     public void setNovice(boolean novice) {
-        m_settings.setBooleanProperty( NOVICE, novice);
+        m_settings.setBooleanProperty(m_singleton.NOVICE, novice);
     }
     
 	//#ifdef DLOGGING
     /** Get log level */
     public String getLogLevel() {
-        String logLevel = m_settings.getStringProperty(0, LOG_LEVEL, "");
+        String logLevel = m_settings.getStringProperty(0, m_singleton.LOG_LEVEL, "");
         return logLevel;
     }
     
     /** Set import URL password */
     public void setLogLevel(String logLevel) {
-        m_settings.setStringProperty( LOG_LEVEL, logLevel);
+        m_settings.setStringProperty(m_singleton.LOG_LEVEL, logLevel);
     }
 	//#endif
     
     /** Get settings version */
     public String getSettingsVers() {
-        return m_settings.getStringProperty( 0, Settings.SETTINGS_NAME, "");
+        return m_settings.getStringProperty( 0, m_settings.SETTINGS_NAME, "");
     }
     
     public Throwable getLoadExc() {
