7e020a8ad00727f49ec96ee3fada993328134f22
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-01-18 04:11:49 +0000

    Read changes in change log or do a diff and look at the comments at the top.

--- testpresrc/com/substanceofcode/jmunit/rssreader/businessentities/RssFeedStoreStrTest.java
+++ testpresrc/com/substanceofcode/jmunit/rssreader/businessentities/RssFeedStoreStrTest.java
@@ -28,8 +28,15 @@
  * IB 2010-06-29 1.11.5RC2 Use ObservableHandler, Observer, and Observable re-written to use observer pattern without GPL code.  This is dual licensed as GPL and LGPL.
  * IB 2010-06-29 1.11.5RC2 Don't use midlet in makeObserable.
  * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
+ * IB 2011-01-14 1.11.5Alpha15 Only compile code/features present only in the full version if this compile is for the full version.
+ * IB 2011-01-14 1.11.5Alpha15 Use notifyAll to avoid waiting with wait.
+ * IB 2011-01-14 1.11.5Alpha15 Remove unused and now obsolete cldc10.TestCase
 */
 
+// Expand to define full vers define
+@DFULLVERSDEF@
+// Expand to define full vers define
+@DINTLINKDEF@
 // Expand to define MIDP define
 @DMIDPVERS@
 // Expand to define test define
@@ -45,21 +52,23 @@ package com.substanceofcode.jmunit.rssreader.businessentities;
 import java.util.Date;
 import java.util.Vector;
 
-import jmunit.framework.cldc10.TestCase;
-
 import com.substanceofcode.jmunit.utilities.BaseTestCase;
 
 import com.substanceofcode.rssreader.businessentities.RssItunesFeed;
 import com.substanceofcode.rssreader.businessentities.RssItunesItem;
+//#ifdef DFULLVERS
 import com.substanceofcode.rssreader.businesslogic.RssFeedParser;
 //#ifdef DMIDP20
 import net.yinlight.j2me.observable.Observer;
 import net.yinlight.j2me.observable.Observable;
 //#endif
+//#endif
 
 final public class RssFeedStoreStrTest extends BaseTestCase
+//#ifdef DFULLVERS
 //#ifdef DMIDP20
 implements Observer
+//#endif
 //#endif
 {
 
@@ -68,7 +77,11 @@ implements Observer
 	//#endif
 
 	public RssFeedStoreStrTest() {
+		//#ifdef DFULLVERS
 		super(7, "RssFeedStoreStrTest");
+		//#else
+		super(6, "RssFeedStoreStrTest");
+		//#endif
 	}
 
 	public void test(int testNumber) throws Throwable {
@@ -91,9 +104,11 @@ implements Observer
 			case 5:
 				testFeedStoreStr5();
 				break;
+				//#ifdef DFULLVERS
 			case 6:
 				testFeedStoreStr6();
 				break;
+				//#endif
 			default:
 				Exception e = new Exception(
 						"No such test testNumber=" + testNumber);
@@ -105,10 +120,15 @@ implements Observer
 		}
 	}
 
+	//#ifdef DFULLVERS
 	//#ifdef DMIDP20
 	public void changed(Observable observable, Object arg) {
 		ready = true;
+		synchronized(this) {
+			super.notifyAll();
+		}
 	}
+	//#endif
 
 	public boolean isReady() {
 		return ready;
@@ -117,7 +137,7 @@ implements Observer
 	private void waitReady() throws Throwable {
 		while (!isReady()) {
 			synchronized(this) {
-				wait(1000L);
+				wait(500L);
 			}
 		}
 	}
@@ -131,38 +151,38 @@ implements Observer
 			byte explicit2) throws Throwable {
 		try {
 			RssItunesFeed feed = new RssItunesFeed(
-				"test" + nbr + suffix, "url" + nbr + suffix, "username" + nbr + suffix, "password" + nbr + suffix);
+					"test" + nbr + suffix, "url" + nbr + suffix, "username" + nbr + suffix, "password" + nbr + suffix);
 			feed.setUpddate(new Date(ldate).toString());
 			if (isItunes) {
 				feed.modifyItunes(
-					true, "title" + nbr + suffix, "description" + nbr + suffix, "language" + nbr + suffix, "author" + nbr + suffix,
-					"subtitle" + nbr + suffix, "summary" + nbr + suffix,
-					explicit);
+						true, "title" + nbr + suffix, "description" + nbr + suffix, "language" + nbr + suffix, "author" + nbr + suffix,
+						"subtitle" + nbr + suffix, "summary" + nbr + suffix,
+						explicit);
 			}
 			Vector vitems = new Vector();
 			if (isItunes && (litem1Date > 0L)) {
 				RssItunesItem item1 = new RssItunesItem("title" + nbr + suffix, "link" + nbr + suffix, "desc" + nbr + suffix,
 						new Date(litem1Date),
-							"enclosure" + nbr + suffix, true,
-							true,
-							"author" + nbr + suffix,
-							"subtitle" + nbr + suffix,
-							"summary" + nbr + suffix,
-							explicit1,
-							"50:00");
+						"enclosure" + nbr + suffix, true,
+						true,
+						"author" + nbr + suffix,
+						"subtitle" + nbr + suffix,
+						"summary" + nbr + suffix,
+						explicit1,
+						"50:00");
 				vitems.addElement(item1);
 			}
 
 			if (isItunes && (litem2Date > 0L)) {
 				RssItunesItem item2 = new RssItunesItem("title" + nbr + suffix + ".2", "link" + nbr + suffix + ".2", "desc" + nbr + suffix + ".2",
 						new Date(litem2Date),
-							"enclosure" + nbr + suffix + ".2", true,
-							true,
-							"author" + nbr + suffix + ".2",
-							"subtitle" + nbr + suffix + ".2",
-							"summary" + nbr + suffix + ".2",
-							explicit2,
-							"50:20");
+						"enclosure" + nbr + suffix + ".2", true,
+						true,
+						"author" + nbr + suffix + ".2",
+						"subtitle" + nbr + suffix + ".2",
+						"summary" + nbr + suffix + ".2",
+						explicit2,
+						"50:20");
 				vitems.addElement(item2);
 			}
 
@@ -187,9 +207,9 @@ implements Observer
 			byte explicit2) throws Throwable {
 		try {
 			RssItunesFeed feed = feedStoreStrFactory(mname,
-									"1", nbr, suffix + "z",
-									isItunes, ldate, explicit, litem1Date,
-									explicit1, litem2Date, explicit2);
+					"1", nbr, suffix + "z",
+					isItunes, ldate, explicit, litem1Date,
+					explicit1, litem2Date, explicit2);
 
 			return feed;
 
@@ -207,12 +227,12 @@ implements Observer
 		try {
 			long cdate = System.currentTimeMillis();
 			RssItunesFeed feed = feedStoreStrTestSub(mname, 1, "",
-			false, cdate, RssItunesItem.convExplicit(""),
-			0L, RssItunesItem.convExplicit(""), 0L,
-			RssItunesItem.convExplicit(""));
+					false, cdate, RssItunesItem.convExplicit(""),
+					0L, RssItunesItem.convExplicit(""), 0L,
+					RssItunesItem.convExplicit(""));
 			storeStringTestSub(mname, true, true, true, true, true,
 					feed, null);
-			
+
 		} catch (Throwable e) {
 			//#ifdef DLOGGING
 			logger.severe(mname + " failure ",e);
@@ -228,9 +248,9 @@ implements Observer
 			long cdate = System.currentTimeMillis() + 3000L;
 			RssItunesFeed feed = feedStoreStrTestSub(mname, 2,
 					"",
-			true, cdate, RssItunesItem.convExplicit("yes"),
-			0L, RssItunesItem.convExplicit(""), 0L,
-			RssItunesItem.convExplicit(""));
+					true, cdate, RssItunesItem.convExplicit("yes"),
+					0L, RssItunesItem.convExplicit(""), 0L,
+					RssItunesItem.convExplicit(""));
 
 			storeStringTestSub(mname, true, true, true, true, true,
 					feed, null);
@@ -249,9 +269,9 @@ implements Observer
 		try {
 			long cdate = System.currentTimeMillis();
 			RssItunesFeed feed = feedStoreStrTestSub(mname, 2, "",
-			true, cdate, RssItunesItem.convExplicit("yes"),
-			cdate + 5500L, RssItunesItem.convExplicit("no"), 0L,
-			RssItunesItem.convExplicit(""));
+					true, cdate, RssItunesItem.convExplicit("yes"),
+					cdate + 5500L, RssItunesItem.convExplicit("no"), 0L,
+					RssItunesItem.convExplicit(""));
 
 			storeStringTestSub(mname, true, true, true, true, true,
 					feed, null);
@@ -270,9 +290,9 @@ implements Observer
 		try {
 			long cdate = System.currentTimeMillis() + 3000L;
 			RssItunesFeed feed = feedStoreStrTestSub(mname, 4, "",
-			true, cdate, RssItunesItem.convExplicit("yes"),
-			cdate + 5500L, RssItunesItem.convExplicit("no"), cdate + 6600L,
-			(byte)0);
+					true, cdate, RssItunesItem.convExplicit("yes"),
+					cdate + 5500L, RssItunesItem.convExplicit("no"), cdate + 6600L,
+					(byte)0);
 
 			storeStringTestSub(mname, true, true, true, true, true,
 					feed, null);
@@ -290,9 +310,9 @@ implements Observer
 		try {
 			long cdate = System.currentTimeMillis() + 3000L;
 			RssItunesFeed feed = feedStoreStrTestSub(mname, 4, "b",
-			true, cdate, (byte)1,
-			cdate + 5500L, RssItunesItem.convExplicit("yes"), cdate + 8800L,
-			RssItunesItem.convExplicit("no"));
+					true, cdate, (byte)1,
+					cdate + 5500L, RssItunesItem.convExplicit("yes"), cdate + 8800L,
+					RssItunesItem.convExplicit("no"));
 
 			storeStringTestSub(mname, true, true, true, true, true,
 					feed, null);
@@ -311,9 +331,9 @@ implements Observer
 		try {
 			long cdate = System.currentTimeMillis() + 3000L;
 			RssItunesFeed feed = feedStoreStrTestSub(mname, 5, "",
-			true, cdate, RssItunesItem.convExplicit("yes"),
-			cdate + 5500L, RssItunesItem.convExplicit("no"), cdate + 7000L,
-			(byte)2);
+					true, cdate, RssItunesItem.convExplicit("yes"),
+					cdate + 5500L, RssItunesItem.convExplicit("no"), cdate + 7000L,
+					(byte)2);
 
 			storeStringTestSub(mname, true, true, true, true, true,
 					feed, null);
@@ -326,13 +346,14 @@ implements Observer
 		}
 	}
 
+	//#ifdef DFULLVERS
 	public void testFeedStoreStr6() throws Throwable {
 		String mname = "testFeedStoreStr6";
 		try {
 			RssItunesFeed feed = new RssItunesFeed(
-				"test3", "jar:///rss-1252.xml", "", "");
+					"test3", "jar:///rss-1252.xml", "", "");
 
-			RssFeedParser fparser = new RssFeedParser(feed);
+			RssFeedParser fparser = new RssFeedParser(feed, null, false);
 			//#ifdef DMIDP20
 			fparser.makeObserable(true, 10);
 			ready = false;
@@ -367,8 +388,9 @@ implements Observer
 			throw e;
 		}
 	}
+	//#endif
 
-    public void storeStringTestSub(final String mname, final boolean saveHdr,
+	public void storeStringTestSub(final String mname, final boolean saveHdr,
 			final boolean serializeItems, final boolean encoded,
 			boolean itemsSaved, boolean modifyCapable,
 			RssItunesFeed feed,
@@ -387,13 +409,13 @@ implements Observer
 			//#endif
 			if (genFeed) {
 				feed = RssItunesFeed.deserialize(true, encoded,
-								storeString);
+						storeString);
 			}
 			//#ifdef DLOGGING
 			if (finestLoggable) {logger.finest("storeStringTestSub  " + mname + " feed=" + feed);}
 			//#endif
 			RssItunesFeed nfeed = RssItunesFeed.deserialize(true, encoded,
-								storeString);
+					storeString);
 			//#ifdef DLOGGING
 			if (finestLoggable) {logger.finest("storeStringTestSub " + mname + " nfeed=" + nfeed);}
 			//#endif
