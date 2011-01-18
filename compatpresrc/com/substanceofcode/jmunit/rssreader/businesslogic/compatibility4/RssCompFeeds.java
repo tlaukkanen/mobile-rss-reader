3a140c76ac910f46b516bc4035f2c8c8cc4f357d
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-01-18 04:02:59 +0000

    Read changes in change log or do a diff and look at the comments at the top.

--- compatpresrc/com/substanceofcode/jmunit/rssreader/businesslogic/compatibility4/RssCompFeeds.java
+++ compatpresrc/com/substanceofcode/jmunit/rssreader/businesslogic/compatibility4/RssCompFeeds.java
@@ -24,6 +24,7 @@
  * IB 2010-05-28 1.11.5RC2 Do comparison test using FeedListParsers.xml.
  * IB 2010-09-29 1.11.5Dev8 Add //#preprocess for RIM preprocessor.
  * IB 2010-10-12 1.11.5Dev9 Change to --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
+ * IB 2011-01-14 1.11.5Alpha15 Remove unused and now obsolete cldc10.TestCase
  */
 
 // Expand to define test define
@@ -37,8 +38,6 @@
 package com.substanceofcode.jmunit.rssreader.businesslogic.compatibility4;
 
 import java.util.Date;
-
-import jmunit.framework.cldc10.TestCase;
 
 import com.substanceofcode.rssreader.businessentities.RssItunesFeedInfo;
 
