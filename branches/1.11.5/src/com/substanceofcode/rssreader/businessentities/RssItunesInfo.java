9e5d6b21e57f174d4fab21f9879b7d12a479e699
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-03-14 06:11:09 +0000

    Changes in comments in diff.

---
+++ branches/1.11.5/src/com/substanceofcode/rssreader/businessentities/RssItunesInfo.java
@@ -0,0 +1,54 @@
+//--Need to modify--#preprocess
+/*
+ * RssItunesInfo.java
+ *
+ * Copyright (C) 2005-2006 Tommi Laukkanen
+ * Copyright (C) 2007 Tommi Laukkanen
+ * Copyright (C) 2009 Irving Bunton Jr
+ * http://www.substanceofcode.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ * IB 2011-03-07 1.11.5Dev17 Need RssItunesInfo to move itunes flag to RssItem.
+*/
+
+// Expand to define logging define
+//#define DNOLOGGING
+// Expand to define itunes define
+//#define DNOITUNES
+// Expand to define test define
+//#define DNOTEST
+//#ifdef DTEST
+//@package com.substanceofcode.rssreader.businessentities;
+//@
+//@import java.util.Date;
+//@
+//@/**
+//@ * RssItunesItem class is a data store for a single item in RSS feed.
+//@ * One item consist of title, link, description and optional date.
+//@ *
+//@ * @author  Tommi Laukkanen
+//@ * @version 1.1
+//@ */
+//@public interface RssItunesInfo {
+//@    
+//@    void setItunes(boolean itunes);
+//@
+//@    boolean isItunes();
+//@
+//@}
+//#endif
