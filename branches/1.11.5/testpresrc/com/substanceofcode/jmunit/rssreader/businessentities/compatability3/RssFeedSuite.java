4b1a396ff7d8b379e1f74e59f65140cf5f03384a
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-03-14 06:13:09 +0000

    Changes in comments in diff.

---
+++ branches/1.11.5/testpresrc/com/substanceofcode/jmunit/rssreader/businessentities/compatability3/RssFeedSuite.java
@@ -0,0 +1,59 @@
+//--Need to modify--#preprocess
+/*
+ * RssFeedSuite.java
+ *
+ * Copyright (C) 2009 Irving Bunton
+ * http://code.google.com/p/mobile-rss-reader/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ * IB 2010-05-24 1.11.5RC2 Move compatibility tests to compatibility packages.
+ * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
+ * IB 2011-01-14 1.11.5Alpha15 Only compile this if it is the full version.
+ * IB 2011-01-14 1.11.5Alpha15 Use conditional preprocessed cldc11 code with modifications instead of cldc10 code.
+ */
+
+// Expand to define full vers define
+@DFULLVERSDEF@
+// Expand to define full vers define
+@DINTLINKDEF@
+// Expand to define test define
+@DTESTDEF@
+// Expand to define compatibility
+@DCOMPATDEF@
+// Expand to define JMUnit test define
+@DJMTESTDEF@
+// Expand to define logging define
+@DLOGDEF@
+
+//#ifdef DJMTEST
+//#ifdef DFULLVERS
+//#ifdef DCOMPATIBILITY
+package com.substanceofcode.jmunit.rssreader.businessentities.compatibility3;
+
+import jmunit.framework.cldc11.TestSuite;
+
+final public class RssFeedSuite extends TestSuite {
+
+	public RssFeedSuite() {
+		super("compatibility3.RssFeedSuite");
+		add(new com.substanceofcode.jmunit.rssreader.businessentities.compatibility3.RssFeedStoreTest());
+	}
+}
+//#endif
+//#endif
+//#endif
