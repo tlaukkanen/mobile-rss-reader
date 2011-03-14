2a6a01b0a423e056f161366e7cfaf2e54d1975bc
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-03-14 06:10:59 +0000

    Changes in comments in diff.

--- src/com/substanceofcode/rssreader/businessentities/RssItem.java
+++ src/com/substanceofcode/rssreader/businessentities/RssItem.java
@@ -28,6 +28,10 @@
  * IB 2010-11-26 1.11.5Dev15 Use checkRead to set the m_unreadItem to the parameter RssItem's m_unreadItem if the other fields are equal.
  * IB 2010-11-26 1.11.5Dev15 Use itemEquals to compare each item for testing and logging.
  * IB 2011-01-24 1.11.5Dev16 Don't compile unneeded code for internet link version.
+ * IB 2011-02-01 1.11.5Dev17 Need clone method for RSS items.
+ * IB 2011-03-06 1.11.5Dev17 Combine statements.
+ * IB 2011-03-06 1.11.5Dev17 Have checkRead return true if the item's unread was saved.
+ * IB 2011-03-07 1.11.5Dev17 Have m_state keep track of unread items and itunes flag.
  */
 
 // Expand to define itunes define
@@ -74,7 +78,7 @@ import java.util.Date;
 public class RssItem
 	//#ifdef DTEST
 	//#ifdef DJMTEST
-//@	implements RssItemInfo
+//@	implements RssItemInfo, RssItunesInfo
 	//#endif
 	//#endif
 {
@@ -87,7 +91,7 @@ public class RssItem
     protected String m_desc  = "";   // The RSS item description
     protected Date m_date = null;
     protected String m_enclosure  = "";   // The RSS item enclosure
-    protected boolean m_unreadItem = false;
+    protected byte m_state = (byte)0x00;
 	//#ifdef DLOGGING
 //@    private Logger logger = Logger.getLogger("RssItem");
 //@    private boolean fineLoggable = logger.isLoggable(Level.FINE);
@@ -106,19 +110,25 @@ public class RssItem
 	}
 
     /** Creates a new instance of RssItem */
-    public RssItem(String title, String link, String desc, Date pubDate,
-			       String enclosure, boolean unreadItem) {
+    protected RssItem(String title, String link, String desc, Date pubDate,
+			       String enclosure, byte state) {
         m_title = title;
         m_link = link;
         m_desc = desc;
         m_date = pubDate;
         m_enclosure = enclosure;
-        m_unreadItem = unreadItem;
+        m_state = state;
+    }
+    
+    public RssItem(String title, String link, String desc, Date pubDate,
+			       String enclosure, boolean unreadItem) {
+		this(title, link, desc, pubDate, enclosure,
+				(byte)(unreadItem ? 0x01 : 0x00));
     }
     
     public RssItem(RssItem item) {
 		this(item.m_title, item.m_link, item.m_desc, item.m_date,
-			 item.m_enclosure, item.m_unreadItem);
+			 item.m_enclosure, item.m_state);
 	}
 
     /** Get RSS item title */
@@ -177,7 +187,7 @@ public class RssItem
 
 		String title = m_title.replace('|', CONE);
         String preData = title + "|" + m_link + "|" + dateString + "|" +
-			    m_enclosure + "|" + (m_unreadItem ? "1" : "0") + "|" + m_desc;
+			    m_enclosure + "|" + (int)m_state + "|" + m_desc;
 		return (preData);
 	}
     
@@ -211,11 +221,13 @@ public class RssItem
 			/* Node count should be 6:
 			 * title | link | date | enclosure | unreadItem | desc
 			 */
-			int TITLE = 0;
+			{
+				int TITLE;
+				m_title = nodes[startIndex + (TITLE = 0)];
+			}
 			//#ifdef DLOGGING
-//@			if (finestLoggable) {logger.finest("currCmpRssfeeds startIndex,nodes.length,first nodes=" + startIndex + "," + nodes.length + "|" + nodes[ startIndex + TITLE ]);} ;
+//@			if (finestLoggable) {logger.finest("currCmpRssfeeds startIndex,nodes.length,first nodes=" + startIndex + "," + nodes.length + "|" + m_title);} ;
 			//#endif
-			m_title = nodes[startIndex + TITLE];
 			if (hasPipe) {
 				if (iTunesCapable) {
 					m_title = m_title.replace(CONE, '|');
@@ -224,32 +236,42 @@ public class RssItem
 				}
 			}
 			
-			int LINK = 1;
-			m_link = nodes[startIndex + LINK];
+			{
+				int LINK;
+				m_link = nodes[startIndex + (LINK = 1)];
+			}
 			
-			int DATE = 2;
-			String dateString = nodes[startIndex + DATE];
-			if(dateString.length()>0) {
-				if (iTunesCapable) {
-					m_date = new Date(Long.parseLong(dateString, 16));
-				} else {
-					m_date = new Date(Long.parseLong(dateString));
-				}
-			}        
+			{
+				int DATE;
+				String dateString = nodes[startIndex + (DATE = 2)];
+				if(dateString.length()>0) {
+					if (iTunesCapable) {
+						m_date = new Date(Long.parseLong(dateString, 16));
+					} else {
+						m_date = new Date(Long.parseLong(dateString));
+					}
+				}        
+			}
 			
-			int ENCLOSURE = 3;
-			m_enclosure = nodes[startIndex + ENCLOSURE];
+			{
+				int ENCLOSURE;
+				m_enclosure = nodes[startIndex + (ENCLOSURE = 3)];
+			}
 
-			int NEWITEM = 4;
-			String cunreadItem = nodes[startIndex + NEWITEM];
-			m_unreadItem = (cunreadItem.equals("1"));
+			{
+				int STATE;
+				String cunreadState = nodes[startIndex + (STATE = 4)];
+				m_state = (byte)Integer.parseInt(cunreadState);
+			}
 					
 			// If description has '|', we need to join.
-			int DESC = 5;
-			if (DESC + startIndex < (nodes.length - 1)) {
-				m_desc = MiscUtil.join(nodes, "|", startIndex + DESC);
-			} else {
-				m_desc = nodes[startIndex + DESC];
+			{
+				int DESC;
+				if ((DESC = 5) + startIndex < (nodes.length - 1)) {
+					m_desc = MiscUtil.join(nodes, "|", startIndex + DESC);
+				} else {
+					m_desc = nodes[startIndex + DESC];
+				}
 			}
 					
         } catch(Exception e) {
@@ -290,7 +312,7 @@ public class RssItem
         return item;
 	}
 
-	public void checkRead(RssItem item) {
+	public boolean checkRead(RssItem item) {
 		//#ifdef DLOGGING
 //@		if (finestLoggable) {logger.finest("checkRead item=" + item);}
 //@		if (finestLoggable) {logger.finest("checkRead this=" + this);}
@@ -301,9 +323,12 @@ public class RssItem
 			MiscUtil.cmpDateStr(m_date, item.m_date) ||
 			m_enclosure.equals(item.m_enclosure)) {
 			//#ifdef DLOGGING
-//@			if (finestLoggable) {logger.finest("checkRead m_title,m_unreadItem,item.m_unreadItem=" + m_title + "," + m_unreadItem + "," + item.m_unreadItem);}
+//@			if (finestLoggable) {logger.finest("checkRead m_title,m_state,item.m_state=" + m_title + "," + m_state + "," + item.m_state);}
 			//#endif
-			m_unreadItem = item.m_unreadItem;
+			m_state = item.m_state;
+			return true;
+		} else {
+			return false;
 		}
 	}
 
@@ -326,11 +351,27 @@ public class RssItem
 	//#endif
 
     public void setUnreadItem(boolean unreadItem) {
-        this.m_unreadItem = unreadItem;
+        this.m_state = (byte)((m_state & 0x0e) | (unreadItem ? 0x01 : 0x00));
     }
 
     public boolean isUnreadItem() {
-        return (m_unreadItem);
+        return ((m_state & 0x01) == 1);
+    }
+
+    public void setItunes(boolean itunes) {
+		//#ifdef DITUNES
+//@        this.m_state = (byte)((m_state & 0x0d) | (itunes ? 0x02 : 0x00));
+		//#else
+        this.m_state &= 0x0d;
+		//#endif
+    }
+
+    public boolean isItunes() {
+		//#ifdef DITUNES
+//@        return ((m_state & 0x02) == 0x02);
+		//#else
+        return (false);
+		//#endif
     }
 
     public String getEnclosure() {
@@ -345,10 +386,14 @@ public class RssItem
 	//#ifdef DTEST
 //@    public String toString() {
 //@        String preData = m_title + "|" + m_link + "|" + m_date + "|" +
-//@			    m_enclosure + "|" + m_unreadItem + "|" + m_desc;
+//@			    m_enclosure + "|" + m_state + "|" + m_desc;
 //@		return (preData);
 //@	}
 	//#endif
     
+    public Object clone() {
+		return new RssItem(this);
+	}
+
 }
 //#endif
