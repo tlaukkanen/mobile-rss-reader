e0303c84c7b6dc658a96bb80e43df0c3fd55254a
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-09-20 00:45:39 +0000

    Changes listed in header comments.

--- branches/1.11.1/testpresrc/com/substanceofcode/jmunit/utils/XmlRequest.java
+++ branches/1.11.1/testpresrc/com/substanceofcode/jmunit/utils/XmlRequest.java
@@ -24,27 +24,33 @@
  * IB 2010-03-14 1.11.5RC2 Test results from XmlParser.
  * IB 2010-05-29 1.11.5RC2 Return first non PROLOGUE, DOCTYPE, STYLESHEET, or ELEMENT which is not link followed by meta.
  * IB 2010-10-12 1.11.5Dev9 Add --Need to modify--#preprocess to modify to become //#preprocess for RIM preprocessor.
+ * IB 2011-01-14 1.11.5Alpha15 Only compile this if it is the full version.
 */
 
+// Expand to define full vers define
+@DFULLVERSDEF@
+// Expand to define full vers define
+@DINTLINKDEF@
 // Expand to define test define
 @DTESTDEF@
 // Expand to define JMUnit test define
 @DJMTESTDEF@
 
 //#ifdef DJMTEST
+//#ifdef DFULLVERS
 package com.substanceofcode.jmunit.utils;
 
 public interface XmlRequest
 {
-	static int GET_PARSE = 0;
-	static int GET_PARSE_XML_ELEMENT = 1;
-	static int GET_IS_UTF =	2;
-	static int GET_IS_WIN = 3;
-	static int GET_IS_DOCENCODING = 4;
-	static int GET_NAMESPACES = 5;
-	static int GET_ATTRIBUTE = 6;
-	static int GET_TEXT = 7;
-	static int GET_NAME = 8;
+	int GET_PARSE = 0;
+	int GET_PARSE_XML_ELEMENT = 1;
+	int GET_IS_UTF =	2;
+	int GET_IS_WIN = 3;
+	int GET_IS_DOCENCODING = 4;
+	int GET_NAMESPACES = 5;
+	int GET_ATTRIBUTE = 6;
+	int GET_TEXT = 7;
+	int GET_NAME = 8;
 	static Integer IGET_PARSE = new Integer(GET_PARSE);
 	static Integer IGET_PARSE_XML_ELEMENT = new Integer(GET_PARSE_XML_ELEMENT);
 	static Integer IGET_IS_UTF =	new Integer(GET_IS_UTF);
@@ -55,4 +61,5 @@ public interface XmlRequest
 	static Integer IGET_TEXT = new Integer(GET_TEXT);
 	static Integer IGET_NAME = new Integer(GET_NAME);
 }
+//#endif
 //#endif
