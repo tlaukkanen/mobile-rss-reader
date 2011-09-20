eb085094ac1ead7b8468d39d662834e194fdc1b9
ibuntonjr <ibuntonjr@87849a4e-271b-0410-afa3-4dad9808a5ad>
2011-09-20 00:40:18 +0000

    Changes listed in header comments.

---
+++ presrc/com/substanceofcode/utils/EncodingUtilIntr.java
@@ -0,0 +1,61 @@
+//--Need to modify--#preprocess
+/*
+ * EncodingUtilIntr.java
+ *
+ * Copyright (C) 2005-2006 Tommi Laukkanen
+ * Copyright (C) 2007-2010 Irving Bunton, Jr
+ * http://www.substanceofcode.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ * IB 2010-01-01 1.11.5Dev15 Class to allow testing of encoding util (future).
+ * IB 2011-01-14 1.11.5Alpha15 Only compile this if it is the full version.
+ */
+
+// Expand to define full vers define
+@DFULLVERSDEF@
+// Expand to define full vers define
+@DINTLINKDEF@
+// Expand to define testing define
+@DTESTDEF@
+// Expand to define logging define
+@DLOGDEF@
+//#ifdef DTEST
+//#ifdef DFULLVERS
+package com.substanceofcode.utils;
+
+/**
+ * Simple encoding handler to allow handling utf-16 and 1252.
+ *
+ * @author Irving Bunton Jr
+ */
+public interface EncodingUtilIntr {
+    
+    public void getEncoding(final String fileEncoding, final String encoding);
+
+    public String getBitEncoding(final byte bitNbrDoc);
+
+    public void getEncoding(final boolean hasIso8859Encoding,
+			final String isoEncoding, final boolean hasWinEncoding,
+			final String winEncoding, final String fileEncoding,
+			final String encoding);
+
+    public byte getBitNbrDoc();
+
+}
+//#endif
+//#endif
